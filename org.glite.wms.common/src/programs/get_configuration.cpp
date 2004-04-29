bbd00f4861dc1ccd3fb3322e4c93c2789a84e605
Alberto di Meglio <Alberto.Di.Meglio@cern.ch>
2004-04-29 23:16:30 +0000

    Added programs subcomponent: merged from EDG Modified include statements in files removing full path: WARNING work in progress

---
+++ org.glite.wms.common/src/programs/get_configuration.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <memory>
+
+#include <classad_distribution.h>
+
+#include "../configuration/Configuration.h"
+#include "../configuration/exceptions.h"
+
+USING_COMMON_NAMESPACE;
+using namespace std;
+
+int main( int argc, char *argv[] )
+try {
+  int                               ret = 0;
+  const classad::ClassAd           *ad;
+  const classad::ExprList          *lst;
+  configuration::Configuration      config( "edg_wl.conf", "LogMonitor" );
+  auto_ptr<classad::ClassAd>        complete( config.get_classad() );
+  classad::Value                    value;
+  classad::PrettyPrint              unp;
+  string                            buffer;
+
+  if( argc == 2 ) {
+    complete->EvaluateExpr( string(argv[1]), value );
+
+    switch( value.GetType() ) {
+    case classad::Value::NULL_VALUE:
+    case classad::Value::ERROR_VALUE:
+    case classad::Value::UNDEFINED_VALUE:
+    default:
+      ret = 1;
+      break;
+    case classad::Value::BOOLEAN_VALUE:
+    case classad::Value::INTEGER_VALUE:
+    case classad::Value::REAL_VALUE:
+    case classad::Value::STRING_VALUE:
+      cout << value << endl;
+      break;
+    case classad::Value::CLASSAD_VALUE:
+      value.IsClassAdValue( ad );
+      cout << *ad << endl;
+      break;
+    case classad::Value::LIST_VALUE:
+      value.IsListValue( lst );
+
+      unp.Unparse( buffer, lst );
+      cout << buffer << endl;
+
+      break;
+    }
+  }
+  else if( argc < 2 )
+    cout << *complete << endl;
+  else ret = 1;
+
+  return ret;
+}
+catch( configuration::CannotConfigure &error ) {
+  cerr << error << endl;
+
+  return 1;
+}
