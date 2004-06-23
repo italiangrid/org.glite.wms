ffb4c72ded67f64887f1f1940fa18d6f9e549209
Salvatore Monforte <salvatore.monforte@ct.infn.it>
2004-06-23 09:40:03 +0000

    Initial revision

---
+++ org.glite.wms.rls/src/ReplicaServiceException.cpp
@@ -0,0 +1,109 @@
+/***************************************************************************
+ *  filename  : ReplicaServiceException.cpp
+ *  authors   : Elisabetta Ronchieri <elisbetta.ronchieri@cnaf.infn.it>
+ *  Copyright (c) 2002 CERN and INFN on behalf of the EU DataGrid.
+ *  For license conditions see LICENSE file or
+ *  http://www.edg.org/license.html
+ ***************************************************************************/
+
+#include <string>
+
+#include "edg/workload/planning/broker/rls/ReplicaServiceException.h"
+
+using namespace std;
+
+namespace edg {
+namespace workload {
+namespace planning {
+namespace broker {
+namespace rls {
+
+ReplicaServiceException::ReplicaServiceException(void) 
+  : exception()
+{
+}
+
+ReplicaServiceException::~ReplicaServiceException(void) throw()
+{
+}
+
+const char* 
+ReplicaServiceException::what(void) const throw()
+{
+  return reason().c_str();
+}
+
+VoException::VoException(const string& par)
+  : m_voex_parameter(par)
+{
+}
+
+VoException::~VoException(void) throw()
+{
+}
+
+string 
+VoException::reason(void) const
+{
+  string error("Vo is wrong!");
+  error.append(m_voex_parameter);
+  return error;
+}
+
+const string& 
+VoException::parameter(void) const
+{
+  return m_voex_parameter;
+}
+
+LfnException::LfnException(const string& par)
+  : m_lfnex_parameter(par)
+{
+}
+
+LfnException::~LfnException(void) throw()
+{
+}
+
+string
+LfnException::reason(void) const
+{
+  string error("Lfn is wrong!");
+  error.append(m_lfnex_parameter);
+  return error;
+}
+
+const string&
+LfnException::parameter(void) const
+{
+  return m_lfnex_parameter;
+}
+
+InvalidRLS::InvalidRLS(const string& par)
+  : m_rlsex_parameter(par)
+{
+}
+
+InvalidRLS::~InvalidRLS(void) throw()
+{
+}
+
+string 
+InvalidRLS::reason(void) const
+{
+  string error("Cannot : ");
+  error.append(m_rlsex_parameter);
+  return error;
+}
+
+const string& 
+InvalidRLS::parameter(void) const
+{
+  return m_rlsex_parameter;
+}
+
+} // namespace rls
+} // namespace broker
+} // namespace planning
+} // namespace workload
+} // namespace edg
---
+++ org.glite.wms.rls/src/ReplicaServiceException.h
@@ -0,0 +1,74 @@
+/***************************************************************************
+ *  filename  : ReplicaServiceException.h
+ *  authors   : Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
+ *  Copyright (c) 2002 CERN and INFN on behalf of the EU DataGrid.
+ *  For license conditions see LICENSE file or
+ *  http://www.edg.org/license.html
+ ***************************************************************************/
+
+// $Id$
+
+#ifndef GLIDE_WMS_RLS_REPLICASERVICEEXCEPTION_H
+#define GLIDE_WMS_RLS_REPLICASERVICEEXCEPTION_H
+
+#include <exception>
+#include <string>
+
+namespace glide {
+namespace wms {
+namespace rls {
+
+class ReplicaServiceException : public std::exception {
+public:
+  ReplicaServiceException(void);
+  virtual ~ReplicaServiceException(void) throw();
+
+public:
+  virtual std::string reason(void) const = 0;
+  virtual const char* what(void) const throw();
+};
+
+class VoException : public ReplicaServiceException {
+public:
+  VoException(const std::string& par);
+  virtual ~VoException(void) throw();
+
+public:
+  virtual std::string reason(void) const;
+  const std::string& parameter(void) const;
+
+private:
+  std::string m_voex_parameter;
+};
+
+class LfnException : public ReplicaServiceException {
+public:
+  LfnException(const std::string& par);
+  virtual ~LfnException(void) throw();
+
+public:
+  virtual std::string reason(void) const;
+  const std::string& parameter(void) const;
+
+private:
+  std::string m_lfnex_parameter;
+};
+
+class InvalidRLS : public ReplicaServiceException {
+public:
+  InvalidRLS(const std::string& par);
+  virtual ~InvalidRLS(void) throw();
+
+public:
+  virtual std::string reason(void) const;
+  const std::string& parameter(void) const;
+
+private:
+  std::string m_rlsex_parameter;
+};
+
+} // namespace rls
+} // namespace wms
+} // namespace glite
+
+#endif
