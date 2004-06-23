e69a900f9452f92a6811885811a2c109bdd24b24
Salvatore Monforte <salvatore.monforte@ct.infn.it>
2004-06-23 09:36:18 +0000

    Initial revision

---
+++ org.glite.wms.matchmaking/src/matchmaker.h
@@ -0,0 +1,90 @@
+/*
+ * File: MatchMaker.h
+ * Author: Monforte Salvatore <Salvatore.Monforte@ct.infn.it>
+ * Copyright (c) 2001 EU DataGrid.
+ * For license conditions see http://www.eu-datagrid.org/license.html
+ */
+ 
+#ifndef _GLITE_WMS_MATCHMAKING_MATCHMAKER_H_
+#define _GLITE_WMS_MATCHMAKING_MATCHMAKER_H_
+
+#include <boost/utility.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include <string>
+#include <map>
+#include <functional>
+
+namespace classad
+{
+  class ClassAd;
+}
+
+namespace glite {
+namespace wms {
+namespace matchmaking {
+
+class match_info
+{
+public:
+  match_info() : m_rank(0), m_undefined( true ) {}
+  match_info(boost::shared_ptr<classad::ClassAd>& a) : m_rank(0), m_CEAd(a), m_undefined(true) {}
+  ~match_info() {}
+  // Default copy ctor and operator= are ok
+  bool isRankUndefined() const { return m_undefined; }
+  double getRank() const { return m_rank; }
+  void setRank(double r) { m_rank = r; m_undefined = false; }  
+  const classad::ClassAd* getAd() const { return m_CEAd.get(); }
+  
+private:
+  double m_rank;
+  boost::shared_ptr<classad::ClassAd> m_CEAd; 
+  bool m_undefined;
+};
+
+/** Type definition for match_table. */
+typedef std::map<std::string, match_info> match_table_t;
+typedef match_table_t::const_iterator match_const_iterator;
+typedef match_const_iterator match_iterator;
+
+/**
+ * Returns CEid attribute of the pointed element a match_table const iterator.
+ * @param it the costant iterator.
+ * @return the pointed element CEid.
+ */
+inline std::string CEid(match_iterator& it) { return it -> first; }
+
+class MatchMakerImpl : boost::noncopyable
+{
+ public:
+  MatchMakerImpl() {}
+  // TODO: no op if requestAd=0
+  virtual void prefetchCEInfo       (const classad::ClassAd* requestAd, match_table_t& suitableCEs) = 0;
+  virtual void checkRequirement     (const classad::ClassAd* requestAd, match_table_t& suitableCEs, bool use_prefetched_ce_info) = 0;
+  virtual void checkRank            (const classad::ClassAd* requestAd, match_table_t& suitableCEs, bool use_prefetched_ce_info) = 0;
+  virtual ~MatchMakerImpl() {}
+}; 
+
+template<class implementation>
+class MatchMaker : boost::noncopyable
+{ 
+ public:
+  MatchMaker() : m_impl( new implementation ) 
+  {
+  } 
+  ~MatchMaker() {}
+  
+  void prefetchCEInfo       (const classad::ClassAd* requestAd, match_table_t& suitableCEs) { m_impl -> prefetchCEInfo( requestAd, suitableCEs); }
+  void checkRequirement     (const classad::ClassAd* requestAd, match_table_t& suitableCEs, bool use_prefetched_ce_info=false) { m_impl -> checkRequirement( requestAd, suitableCEs, use_prefetched_ce_info); }
+  void checkRank            (const classad::ClassAd* requestAd, match_table_t& suitableCEs, bool use_prefetched_ce_info=false) { m_impl -> checkRank( requestAd, suitableCEs, use_prefetched_ce_info); }
+  
+ private:
+  boost::scoped_ptr<MatchMakerImpl>      m_impl;
+};
+
+}; // namespace matchmaking
+}; // wms
+}; // glite
+
+#endif
---
+++ org.glite.wms.matchmaking/src/utility.h
@@ -0,0 +1,48 @@
+/*
+ * File: utility.h
+ * Author: Monforte Salvatore <Salvatore.Monforte@ct.infn.it>
+ * Copyright (c) 2001 EU DataGrid.
+ * For license conditions see http://www.eu-datagrid.org/license.html
+ */
+
+// $Id$
+
+#ifndef _GLITE_WMS_MATCHMAKING_UTILITY_H_
+#define _GLITE_WMS_MATCHMAKING_UTILITY_H_
+
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include "glite/wms/matchmaking/matchmaker.h"
+
+using namespace std;
+
+namespace glite {
+namespace wms {
+namespace matchmaking {
+
+typedef std::vector< std::pair<match_table_t::key_type,match_table_t::mapped_type> > match_vector_t;
+	
+struct rank_less_than_comparator :
+    public binary_function< match_table_t::value_type&, match_table_t::value_type&, bool>
+{ 
+	 bool operator()(const match_table_t::value_type& a, const match_table_t::value_type& b)
+	 {
+	    return a.second.getRank() < b.second.getRank();
+	 }
+};
+
+struct rank_greater_than_comparator :
+    public binary_function< match_table_t::value_type&, match_table_t::value_type&, bool>
+{
+         bool operator()(const match_table_t::value_type& a, const match_table_t::value_type& b)
+         {
+             return a.second.getRank() > b.second.getRank();
+         }
+};
+
+}; // namespace matchmaking
+}; // namespace wms
+}; // namespace glite
+
+#endif
