6017daabcd61b35b2ef4ceda9705d8057b52bf28
Fabrizio Pacini <fabrizio.pacini@telespazio.com>
2004-08-10 12:42:02 +0000

    fixed namespace for userinterface

--- org.glite.wms-ui.api-cpp/src/userinterface_namespace.h
+++ org.glite.wms-ui.api-cpp/src/userinterface_namespace.h
@@ -1,10 +1,10 @@
 #ifndef __USERINTERFACE_NAMESPACE_H_LOADED
 #define __USERINTERFACE_NAMESPACE_H_LOADED
 #include <string>
-#define USERINTERFACE_NAMESPACE_BEGIN namespace edg { namespace workload { namespace userinterface {
+#define USERINTERFACE_NAMESPACE_BEGIN namespace glite { namespace wms { namespace userinterface {
 #define USERINTERFACE_NAMESPACE_END }}
-#define USING_USERINTERFACE_NAMESPACE using namespace edg::workload::userinterface
-#define USING_USERINTERFACE_NAMESPACE_ADD( last ) using namespace edg::workload::userinterface::##last
+#define USING_USERINTERFACE_NAMESPACE using namespace glite::wms::userinterface
+#define USING_USERINTERFACE_NAMESPACE_ADD( last ) using namespace glite::wms::userinterface::##last
 
 #endif /* __USERINTERFACE_NAMESPACE_H_LOADED */
 
