fbb93d071e8f73e6159563d18b2367a5670a24ab
Elisabetta Ronchieri <elisabetta.ronchieri@cnaf.infn.it>
2004-08-11 08:15:34 +0000

    Updated

--- org.glite.wms.common/interface/glite/wms/common/utilities/result_codes.h
+++ org.glite.wms.common/interface/glite/wms/common/utilities/result_codes.h
@@ -16,35 +16,35 @@ namespace utilities{
   * Result Code
 */
 enum ResultCode {
-          SUCCESS,           //  The requested operation has been completed successfully
-          ACCEPTED,         // The requested operation has been accepted
+  SUCCESS,  //  The requested operation has been completed successfully
+  ACCEPTED, // The requested operation has been accepted
 
-          SUBMISSION_FAILURE,  //  API failed, general RB Exc remapping
-          CANCEL_FAILURE,    //  API failed, general RB Exc remapping
-          GETOUTPUT_FAILURE,    //  API failed, general RB Exc remapping
-          STATUS_FAILURE,    //  API failed, general RB Exc remapping
+  SUBMISSION_FAILURE, //  API failed, general RB Exc remapping
+  CANCEL_FAILURE,     //  API failed, general RB Exc remapping
+  GETOUTPUT_FAILURE,  //  API failed, general RB Exc remapping
+  STATUS_FAILURE,     //  API failed, general RB Exc remapping
 
-          GETOUTPUT_FORBIDDEN, //When trying to retrieve output from a not submitted job
-          CANCEL_FORBIDDEN,  //When trying to cancel a not submitted job
-          STATUS_FORBIDDEN,  //When trying to retrieve status from a not submitted job
-          ALREADY_SUBMITTED,  //submit skipped because Job has been already submitted
+  GETOUTPUT_FORBIDDEN, //When trying to retrieve output from a not submitted job
+  CANCEL_FORBIDDEN,    //When trying to cancel a not submitted job
+  STATUS_FORBIDDEN,    //When trying to retrieve status from a not submitted job
+  ALREADY_SUBMITTED,   //submit skipped because Job has been already submitted
 
-          JOIN_FAILED, //When a pthread_join is waiting for a cored thread
+  JOIN_FAILED, //When a pthread_join is waiting for a cored thread
 
-          OUTPUT_NOT_READY,           //JobNotDoneException
-          FILE_TRANSFER_ERROR,       //SandboxIOException
-          JOB_NOT_FOUND,                  //JobNotFoundException
+  OUTPUT_NOT_READY,    //JobNotDoneException
+  FILE_TRANSFER_ERROR, //SandboxIOException
+  JOB_NOT_FOUND,       //JobNotFoundException
 
-          MARKED_FOR_REMOVAL, //Cancel Method Result
-          GENERIC_FAILURE,             //Cancel Method Result
-          CONDOR_FAILURE,             //Cancel Method Result
+  MARKED_FOR_REMOVAL, //Cancel Method Result
+  GENERIC_FAILURE,    //Cancel Method Result
+  CONDOR_FAILURE,     //Cancel Method Result
 
-          GLOBUS_JOBMANAGER_FAILURE,
+  GLOBUS_JOBMANAGER_FAILURE,
 
-          JOB_ALREADY_DONE,
-          JOB_ABORTED,
-          JOB_CANCELLING,
-          JOB_NOT_OWNER
+  JOB_ALREADY_DONE,
+  JOB_ABORTED,
+  JOB_CANCELLING,
+  JOB_NOT_OWNER
 
 };
 
