43a8b2d5104e9abd224a6855ac6061369ac3156f
Alessandro Maraschini <alessandro.maraschini@telespazio.com>
2004-10-21 15:16:29 +0000

    New Server DAgAd Manipulation file

---
+++ org.glite.wmproxy/src/server/wmpexpdagad.h
@@ -0,0 +1,11 @@
+#ifndef GLITE_WMS_WMPROXY_WMPEXPDAGAD_H
+#define GLITE_WMS_WMPROXY_WMPEXPDAGAD_H
+
+#include "glite/wms/jdl/ExpDagAd.h"
+
+class WMPExpDagAd:glite::wms::jdl::ExpDagAd{
+public:
+
+};
+
+#endif // GLITE_WMS_WMPROXY_WMPEXPDAGAD_H
