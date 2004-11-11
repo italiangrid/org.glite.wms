de2ec5007c527810659cf6cfd2936d877416083b
Alessandro Maraschini <alessandro.maraschini@telespazio.com>
2004-11-11 18:36:08 +0000

    Methods implementation

--- org.glite.wms.wmproxy-api-api-cpp/src/wms_wmproxy_api_cpp.cpp
+++ org.glite.wms.wmproxy-api-api-cpp/src/wms_wmproxy_api_cpp.cpp
@@ -1,200 +1,261 @@
 #include "soapWorkloadManagerProxyProxy.h"
 #include "WorkloadManagerProxy.nsmap"
-#include "api.h"
+#include "wms_wmproxy_api_cpp.h"
 using namespace std;
-/*********************************************************
+namespace glite {
+namespace wms {
+namespace wmproxy-api {
+/*****************************************************************
 soapErrorMng - common soap failure managemente
-**********************************************************/
-void soapErrorMng (const WorkloadManagerProxy &wmp ){
+******************************************************************/
+void soapErrorMng (const WorkloadManagerProxy &wmp){
 	cout << "Operation Failed" << endl;
 	soap_print_fault(wmp.soap, stderr);
-	cout << "----------------" << endl;
 	return "Failure";
 }
-/*********************************************************
+
+void soapAuthentication( const WorkloadManagerProxy &wmp,ConfigContext *cfs)
+	string proxy_file=NULL;
+	if (  cfs!=NULL )
+	if (soap_ssl_client_context(wmp.soap,
+		SOAP_SSL_NO_AUTHENTICATION,
+		proxy_file,// keyfile: required only when client must authenticate to server
+		NULL, // password to read the key file
+		NULL, // optional cacert file to store trusted certificates (needed to verify server)
+		"/etc/grid-security/certificates",// optional capath to direcoty with trusted certificates
+		// if randfile!=NULL: use a file with random data to seed randomness
+		NULL
+	))soapErrorMng(wmp);
+}
+
+
+/*****************************************************************
+jobidSoap2cpp
+Tranform the soap jobid structure into cpp primitive object structure
+******************************************************************/
+JobIdStruct* jobidSoap2cpp (  ns1__JobIdStructType *s_id){
+	JobIdStruct *result = new JobIdStruct ;
+	result->jobid=s_id->id ;
+	result->nodeName=NULL ;
+	if (s_id->name!=NULL) result->nodeName= new string(s_id->name  );
+	else result->nodeName=NULL ;
+	for (unsigned int i = 0 ; i< s_id->childrenJob->size(); i++){
+		result->children.push_back(  jobidSoap2cpp(   s_id->childrenJob[i]  ) ) ;
+	}
+	return result ;
+}
+/*****************************************************************
+listSoap2cpp
+Tranform the soap string&long list structure into cpp primitive object structure
+******************************************************************/
+vector <pair<string , long>> listSoap2cpp (ns1__StringAndLongList s_list){
+	vector <pair<string , long>> result ;
+	std::vector<ns1__StringAndLongType*>s_vect=s_list->file;
+	for (unsigned int i = 0 ; i< s_vect.size() ; i++){
+		result.push_back( pair<string , long> (s_vect[i]->name,(long)(s_vect[i]->size));
+	}
+	return result ;
+}
+/*****************************************************************
+Simple ConfigContext Constructor
+******************************************************************/
+ConfigContext::ConfigContext( std::string p , std::string s):proxy_file(p),service_address(s) {};
+
+
+
+/*===============================================
+		WMPROXY SERVICE METHODS:
+===============================================*/
+/*****************************************************************
 getVersion
-**********************************************************/
-string getVersion(){
+******************************************************************/
+string getVersion(ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__getVersionResponse response;
 	if (wmp.ns1__getVersion(response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		return response.version ;
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 jobRegister
-**********************************************************/
-jobidStruct jobRegister (const string &jdl, const string &delegationId){
+******************************************************************/
+JobIdStruct jobRegister (const string &jdl, const string &delegationId, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__jobRegisterResponse response;
 	if (wmp.ns1__jobRegister(jdl, response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		return jobidSoap2cpp ( response._jobIdStruct ) ;
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 jobSubmit
-**********************************************************/
-jobidStruct jobSubmit(const string &jdl, const string &delegationId){
+******************************************************************/
+JobIdStruct jobSubmit(const string &jdl, const string &delegationId, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__jobSubmitResponse response;
 	if (wmp.ns1__jobSubmit(jdl, delegation, response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		return jobidSoap2cpp ( response._jobIdStruct ) ;
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 jobStart
-**********************************************************/
-void jobStart(const string &jobid){
+******************************************************************/
+void jobStart(const string &jobid, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__jobStartResponse response;
 	if (wmp.ns1__jobStart(jobid, response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		// Ok
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 jobCancel
-**********************************************************/
-void jobCancel(const string &jobid){
+******************************************************************/
+void jobCancel(const string &jobid, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__jobCancelResponse response;
 	if (wmp.ns1__jobCancel(jobid, response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		// Ok
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 getMaxInputSandboxSize
-**********************************************************/
-int getMaxInputSandboxSize(){
+******************************************************************/
+int getMaxInputSandboxSize(ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__getMaxInputSandboxSizeResponse response;
 	if (wmp.ns1__getMaxInputSandboxSize(response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		return response.size;
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 getSandboxDestURI
-**********************************************************/
-string getSandboxDestURI(const string &jobid){
+******************************************************************/
+string getSandboxDestURI(const string &jobid, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__getSandboxDestURIResponse response;
 	if (wmp.ns1__getSandboxDestURI(jobid, response) == SOAP_OK) {
 		return response._path;
-	} else soapErrorMng( wmp) ;
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 getTotalQuota
-**********************************************************/
-pair<long, long> getTotalQuota(){
+******************************************************************/
+pair<long, long> getTotalQuota(ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	pair<long, long> quota;
 	ns1__getTotalQuotaResponse response;
 	if (wmp.ns1__getTotalQuota(response) == SOAP_OK) {
 		quota = make_pair( (long)response.hardLimit, (long)response.softLimit );
 		return quota;
-	} else soapErrorMng( wmp) ;
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 getFreeQuota
-**********************************************************/
-pair<long, long>getFreeQuota(){
+******************************************************************/
+pair<long, long>getFreeQuota(ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__getFreeQuotaResponse response;
 	if (wmp.ns1__getFreeQuota(response) == SOAP_OK) {
 		quota = make_pair( (long)response.hardLimit, (long)response.softLimit );
 		return quota;
-	} else soapErrorMng( wmp) ;
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 jobPurge
-**********************************************************/
-void jobPurge(const string &jobid){
+******************************************************************/
+void jobPurge(const string &jobid, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__jobPurgeResponse response;
 	if (wmp.ns1__jobPurge(jobid, response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		// Ok
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 getOutputFileList
-**********************************************************/
-vector <pair<string , long>> getOutputFileList (const string &jobid){
+******************************************************************/
+vector <pair<string , long>> getOutputFileList (const string &jobid, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__getOutputFileListResponse response;
 	if (wmp.ns1__getOutputFileList(jobid, response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		return listSoap2cpp ( response._OutputFileAndSizeList  );
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 jobListMatch
-**********************************************************/
-vector<string> jobListMatch (const string &jdl){
-
+******************************************************************/
+vector<string> jobListMatch (const string &jdl, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__jobListMatchResponse response;
 	if (wmp.ns1__jobListMatch(jdl, response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		return listSoap2cpp ( response._CEIdAndRankList  );
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 getJobTemplate
-**********************************************************/
-string  getJobTemplate (vector<string > jobType, const string &executable,const string &arguments,const string &requirements,const string &rank){
+******************************************************************/
+string  getJobTemplate (vector<string > jobType, const string &executable,const string &arguments,const string &requirements,const string &rank, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__getJobTemplateResponse response;
 	if (wmp.ns1__getJobTemplate(jobType, executable, arguments, requirements, rank, response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		return response.jdl ;
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 getDAGTemplate
-**********************************************************/
-string getDAGTemplate(dependencyStruct dependencies, const string &requirements,const string &rank){
+******************************************************************/
+string getDAGTemplate(NodeStruct dependencies, const string &requirements,const string &rank, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__getDAGTemplateResponse response;
 	if (wmp.ns1__getDAGTemplate(response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
-
+		return response.jdl ;
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 getCollectionTemplate
-**********************************************************/
-string getCollectionTemplate(int jobNumber, const string &requirements,const string &rank){
+******************************************************************/
+string getCollectionTemplate(int jobNumber, const string &requirements,const string &rank, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__getCollectionTemplateResponse response;
 	if (wmp.ns1__getCollectionTemplate(jobNumber, requirements, rank, response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		return response.jdl ;
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 getIntParametricJobTemplate
-**********************************************************/
-string getIntParametricJobTemplate (vector<string> attributes , int parameters , int start , int step , const string &requirements,const string &rank){
+******************************************************************/
+string getIntParametricJobTemplate (vector<string> attributes , int parameters , int start , int step , const string &requirements,const string &rank, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__getIntParametricJobTemplateResponse response;
 	if (wmp.ns1__getIntParametricJobTemplate(attributes, parameters, start, step, requirements, rank, response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		return response.jdl ;
+	} else soapErrorMng(wmp) ;
 }
-/*********************************************************
+/*****************************************************************
 getStringParametricJobTemplate
-**********************************************************/
-string getStringParametricJobTemplate (vector<string>attributes, vector<string> parameters, const string &requirements,const string &rank){
+******************************************************************/
+string getStringParametricJobTemplate (vector<string>attributes, vector<string> parameters, const string &requirements,const string &rank, ConfigContext cfs){
 	WorkloadManagerProxy wmp;
+	soapAuthentication (wmp, cfs);
 	ns1__getStringParametricJobTemplateResponse response;
 	if (wmp.ns1__getStringParametricJobTemplate(attributes, parameters, requirements, rank, response) == SOAP_OK) {
-
-	} else soapErrorMng( wmp) ;
+		return response.jdl ;
+	} else soapErrorMng(wmp) ;
 }
-
-
-
-
-
-
-
-
-
-
-
-
+} // wmproxy-api namespace
+} // wms namespace
+} // glite namespace
--- org.glite.wms.wmproxy-api-api-cpp/src/wms_wmproxy_api_cpp.h
+++ org.glite.wms.wmproxy-api-api-cpp/src/wms_wmproxy_api_cpp.h
@@ -1,20 +1,54 @@
+#ifndef  GLITE_WMS_WMPROXYAPICPP_H
+#define GLITE_WMS_WMPROXYAPICPP_H
+
 #include <iostream>
 #include <string>
 #include <vector>
-std::string getVersion();
-jobidStruct jobRegister (const std::string &jdl, const std::string &delegationId);
-jobidStruct jobSubmit(const std::string &jdl, const std::string &delegationId);
-void jobStart(const std::string &jobid);
-void jobCancel(const std::string &jobid);
-int getMaxInputSandboxSize();
-std::string  getSandboxDestURI(const std::string &jobid);
-std::pair<long, long> getTotalQuota();
-std::pair<long, long>getFreeQuota();
-void jobPurge(const std::string &jobid);
-std::vector <std::pair<std::string , long>> getOutputFileList (const std::string &jobid);
-std::vector<std::string> jobListMatch (const std::string &jdl);
-std::string  getJobTemplate (std::vector<std::string > jobType, const std::string &executable,const std::string &arguments,const std::string &requirements,const std::string &rank);
-std::string getDAGTemplate(dependencyStruct dependencies, const std::string &requirements,const std::string &rank);
-std::string getCollectionTemplate(int jobNumber, const std::string &requirements,const std::string &rank);
-std::string getIntParametricJobTemplate (std::vector<std::string> attributes , int parameters , int start , int step , const std::string &requirements,const std::string &rank);
-std::string getStringParametricJobTemplate (std::vector<std::string>attributes, std::vector<std::string> parameters, const std::string &requirements,const std::string &rank);
+
+namespace glite {
+namespace wms {
+namespace wmproxy-api {
+
+struct NodeStruct {
+	std::string* name ;
+	std::vector< NodeStruct* > childrenNodes ;
+};
+struct JobIdStruct{
+	std::string jobid ;
+	std::string* nodeName ;
+	std::vector< JobIdStruct* > children ;
+};
+class ConfigContext{
+	public:
+		ConfigContext( std::string p , std::string s);
+	private:
+		std::string proxy_file;
+		std::string service_address;
+};
+
+std::string getVersion(ConfigContext cfs=NULL);
+JobIdStruct jobRegister (const std::string &jdl, const std::string &delegationId, ConfigContext cfs=NULL);
+JobIdStruct jobSubmit(const std::string &jdl, const std::string &delegationId, ConfigContext cfs=NULL);
+
+void jobStart(const std::string &jobid, ConfigContext cfs=NULL);
+void jobCancel(const std::string &jobid, ConfigContext cfs=NULL);
+int getMaxInputSandboxSize(ConfigContext cfs=NULL);
+std::string  getSandboxDestURI(const std::string &jobid, ConfigContext cfs=NULL);
+std::pair<long, long> getTotalQuota(ConfigContext cfs=NULL);
+std::pair<long, long>getFreeQuota(ConfigContext cfs=NULL);
+void jobPurge(const std::string &jobid, ConfigContext cfs=NULL);
+std::vector <std::pair<std::string , long>> getOutputFileList (const std::string &jobid, ConfigContext cfs=NULL);
+std::vector<std::string> jobListMatch (const std::string &jdl, ConfigContext cfs=NULL);
+
+std::string getJobTemplate (std::vector<std::string > jobType, const std::string &executable,const std::string &arguments,const std::string &requirements,const std::string &rank, ConfigContext cfs=NULL);
+std::string getDAGTemplate(NodeStruct dependencies, const std::string &requirements,const std::string &rank, ConfigContext cfs=NULL);
+std::string getCollectionTemplate(int jobNumber, const std::string &requirements,const std::string &rank, ConfigContext cfs=NULL);
+std::string getIntParametricJobTemplate (std::vector<std::string> attributes , int parameters , int start , int step , const std::string &requirements,const std::string &rank, ConfigContext cfs=NULL);
+std::string getStringParametricJobTemplate (std::vector<std::string>attributes, std::vector<std::string> parameters, const std::string &requirements,const std::string &rank, ConfigContext cfs=NULL);
+
+} // wmproxy namespace
+} // wms namespace
+} // glite namespace
+
+#endif
+//EOF
