5259f5539c01866444e2db21514e33a60bab7d15
Alessandro Maraschini <alessandro.maraschini@telespazio.com>
2005-01-03 15:39:53 +0000

    managedir method added

--- org.glite.wmproxy/src/server/wmputils.cpp
+++ org.glite.wmproxy/src/server/wmputils.cpp
@@ -3,14 +3,14 @@
 	See http://public.eu-egee.org/partners/ for details on the copyright holders.
 	For license conditions see the license file or http://www.eu-egee.org/license.html
 */
-
 #include "wmputils.h"
 #include "glite/wmsutils/jobid/JobId.h"
 #include "glite/wmsutils/jobid/manipulation.h"
 #include <iostream>
+// by dirmanagement
+#include <boost/lexical_cast.hpp>
 
 using namespace std;
-	
 void
 waitForSeconds(int seconds)
 {
@@ -53,3 +53,26 @@ to_filename(glite::wmsutils::jobid::JobId j, int level, bool extended_path)
 	return path;
 }
 
+void 
+managedir ( const string &dir , int userid ){
+	// Try to find managedirexecutable
+	char* glite_path = getenv ("GLITE_WMS_LOCATION");
+	string gliteDirmanExe = (glite_path==NULL)?("/opt/glite"):(string(glite_path));
+	gliteDirmanExe += "/bin/glite-wms-dirmanager";
+	// SET UP arguments
+	string arguments ="";
+	arguments += " -c "; // UID
+	arguments += " -g " + boost::lexical_cast<std::string>(getgid()); // GROUP
+	arguments += " -m 0770 "; // MODE
+	arguments += dir; // DIRECTORY
+	string command = gliteDirmanExe  + " " + arguments;
+	string error_msg ="";
+	int exit_value= system(command.c_str());
+	switch (exit_value){
+		case 0:
+			break;
+		default:
+			error_msg ="Unexpected error while launching:\n" + gliteDirmanExe ;
+	}
+	if (exit_value!=0) throw JobOperationException(__FILE__, __LINE__, "managedir()" , WMS_OPERATION_NOT_ALLOWED,error_msg);
+}
--- org.glite.wmproxy/src/server/wmputils.h
+++ org.glite.wmproxy/src/server/wmputils.h
@@ -11,10 +11,15 @@ void waitForSeconds(int seconds);
 /**
  * Transform a Jobid into a valid filename
  * @param j the JobId instance
- * @param level 
+ * @param level
  * @param extended_path
  */
 std::string to_filename(glite::wmsutils::jobid::JobId j, int level = 0,
 	bool extended_path = true);
-
+/**
+* Generate / Manage directory properties
+* @param dir name of the dir to be generated
+* @param userid the id of the user
+*/
+void managedir ( const std::string &dir , int userid );
 #endif // GLITE_WMS_WMPROXY_WMPUTILS_H
