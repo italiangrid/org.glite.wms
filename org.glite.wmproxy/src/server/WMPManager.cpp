f846c7ffec5fa957335762e4603763930e0cc491
Marco Pappalardo <xxx@xxx.xx>
2005-01-26 18:56:35 +0000

    New listmatch adaptation.

--- org.glite.wmproxy/src/server/WMPManager.cpp
+++ org.glite.wmproxy/src/server/WMPManager.cpp
@@ -9,6 +9,7 @@
 
 #include <boost/shared_ptr.hpp>
 #include <boost/scoped_ptr.hpp>
+#include <boost/regex.hpp>
 #include <classad_distribution.h>
 
 #include "WMPManager.h"
@@ -17,6 +18,7 @@
 #include "commands/Command.h"
 #include "commands/CommandFactory.h"
 #include "commands/CommandFactoryServerImpl.h"
+#include "commands/const.h"
 #include "glite/wmsutils/exception/Exception.h"
 #include "glite/wms/common/utilities/classad_utils.h" 
 #include "glite/wms/common/logger/edglog.h" 
@@ -78,21 +80,77 @@ namespace server {
 		        }
 		    
 		      } while( cmd -> execute() && !cmd -> isDone() );
+		      
+		      if (cmdname == "ListJobMatch") {
+                        // Fill result struct with data
+			// cmd -> setParam("jdl", jdl); 
+			std::vector<std::string> temp_list; 
+			if ( cmd -> getParam("MatchResult", temp_list) ) { 
+			  if (temp_list.size() > 1 && temp_list.front() == std::string(GLITE_WMS_WMPMATCHMAKINGERROR)) { 
+			    fault.code = 1; //MATCHMAKINGERR_CODE
+			    fault.message = std::string(GLITE_WMS_WMPMATCHMAKINGERROR);
+			    edglog(critical) << "Error during MatchMaking:\n\t" << temp_list[1] << std::endl; 
+			  } else {
+			    /*
+			    struct StringAndLongType {
+			      std::string name; 
+			      long size; 
+			    }; 
+ 
+			    struct StringAndLongList {
+			      std::vector<StringAndLongType*> *file; 
+			    };
+			    */
+			    
+			    for(std::vector<std::string>::const_iterator it = temp_list.begin();
+				it != temp_list.end(); it++) {
+			      try { 
+ 				static boost::regex  expression( "(\\S.+)\\s=\\s(\\S.+)" ); 
+				boost::smatch        pieces;
+				std::string          ceid, rank;
+				if( boost::regex_match( *it, pieces, expression) ) {
+ 				  ceid.assign  (pieces[1].first, pieces[1].second); 
+				  rank.assign  (pieces[2].first, pieces[2].second); 
+				  StringAndLongType* item;
+				  item -> name.assign(ceid);
+				  item -> size = std::atoi(rank.c_str()); 
+				  ((StringAndLongList*)result)->file->push_back( item );
+				} 
+			      } 
+			      catch( boost::bad_expression& e ) { 
+				edglog(critical) << e.what() << std::endl; 
+			      } 
+			    } 
 
-		      // Here we should log the attribute list returned.
-		      // If Submit
+			  }
+			} else { 
+			  // l.push_back(std::string(EDG_WL_NSMATCHMAKINGERROR)); 
+			  // l.push_back(std::string("Unknown Error. No MatchResult: please check.")); 
+			  edglog(critical) << "Error during MatchMaking:\n\tUnknown Error. No MatchResult: please check." << std::endl; 
+			  fault.code = 2; //MATCHMAKING_NORESULT_CODE
+			  fault.message = std::string(GLITE_WMS_WMPMATCHMAKINGERROR);
+			  edglog(critical) << "Error during MatchMaking:\n\tUnknown Error. No MatchResult: please check." << std::endl; 
+			} 
 
-		      cmd -> setParam("Ciccio", "PincoPallo");
-		      /*
-		      struct JobIdStructType { 
-			std::string id;
-			std::string *name;
-			std::vector<JobIdStructType*> *childrenJob;
-		      }; 
-		      */
-		      cmd -> getParam("JobId", ((jobSubmitResponse*)result)->jobIdStruct->id);
-		      fault.code=0;
-		      fault.message="ciao pacio";
+
+                      } else if (cmdname == "JobSubmit" || cmdname == "DagSubmit") {
+                        // Fill result struct with data
+                      } else if (cmdname == "JobCancel") {
+                        // Fill result struct with data
+                        cmd -> setParam("Ciccio", "PincoPallo");
+		        cmd -> getParam("JobId", ((jobSubmitResponse*)result)->jobIdStruct->id);
+                      }
+		      
+                      // Here we should log the attribute list returned.
+		      // For any command possible
+                      if (cmd -> isDone() && cmdname != "ListJobMatch") {
+		        fault.code = 0;
+		        fault.message="Success";
+                      } else {
+                        //An Error happened
+                        fault.code = 1;
+                        fault.message = "Error Occurred";
+                      }
 	              return fault;
 
 	      } 
