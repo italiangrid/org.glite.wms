ea747700ba6f4eaf7c05eb4431d9b0d844db8b22
Giuseppe Avellino <xxx@xxx.xx>
2005-02-03 10:41:45 +0000

    exception handling

---
+++ org.glite.wmproxy/src/utilities/wmpexceptions.cpp
@@ -0,0 +1,48 @@
+/*
+	Copyright (c) Members of the EGEE Collaboration. 2004.
+	See http://public.eu-egee.org/partners/ for details on the copyright holders.
+	For license conditions see the license file or http://www.eu-egee.org/license.html
+*/
+
+#include "wmpexceptions.h"
+
+//namespace glite {
+//namespace wms {
+//namespace wmproxy {
+
+using namespace std;
+using namespace glite::wmsutils::exception;
+
+
+
+JobException::JobException(const std::string& file, int line,
+	const std::string& method, int code, const std::string& exception_name)
+	: Exception(file, line, method, code, exception_name)
+{}
+
+JobTimeoutException::JobTimeoutException(const std::string& file, int line,
+	const std::string& method, int code)
+	: JobException(file, line, method, code, "JobTimeoutException")
+{
+	error_message = "Submit notification timeout expired";
+}
+
+JobOperationException::JobOperationException(const std::string& file, int line,
+	const std::string& method, int code, const std::string& reason)
+	:JobException(file, line, method, code, "JobOperationException")
+    {
+	error_message = "The Operation is not allowed: " + reason;
+	//error_message += reason;
+}
+
+ProxyOperationException::ProxyOperationException(const std::string& file, int line,
+	const std::string& method, int code, const std::string& reason)
+	:JobException(file, line, method, code, "JobOperationException")
+    {
+	error_message = "The Operation is not allowed: " + reason;
+	//error_message += reason;
+}
+
+//} // wmproxy
+//} // wms
+//} // glite
---
+++ org.glite.wmproxy/src/utilities/wmpexceptions.h
@@ -0,0 +1,47 @@
+/*
+	Copyright (c) Members of the EGEE Collaboration. 2004.
+	See http://public.eu-egee.org/partners/ for details on the copyright holders.
+	For license conditions see the license file or http://www.eu-egee.org/license.html
+*/
+
+#ifndef GLITE_WMS_WMPROXY_WMPEXCEPTIONS_H
+#define GLITE_WMS_WMPROXY_WMPEXCEPTIONS_H
+
+#include "wmpexception_codes.h"
+#include "glite/wmsutils/exception/Exception.h"
+
+//namespace glite {
+//namespace wms {
+//namespace wmproxy {
+
+class JobException : public glite::wmsutils::exception::Exception {
+	protected:
+	JobException(const std::string& file, int line, const std::string& method,
+		int code, const std::string& exception_name);
+};
+
+
+class JobTimeoutException : public JobException {
+	public:
+	JobTimeoutException(const std::string& file, int line,
+		const std::string& method, int code);
+};
+
+
+class JobOperationException : public JobException {
+	public:
+	JobOperationException(const std::string& file, int line, 
+		const std::string& method, int code, const std::string& reason);
+};
+
+class ProxyOperationException : public JobException {
+	public:
+	ProxyOperationException(const std::string& file, int line, 
+		const std::string& method, int code, const std::string& reason);
+};
+
+//} // wmproxy
+//} // wms
+//} // glite
+
+#endif // GLITE_WMS_WMPROXY_WMPEXCEPTIONS_H
