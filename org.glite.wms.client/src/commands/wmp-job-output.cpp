4695c90f840c1f3ed1b923766da92d4f4932941d
Marco Sottilaro <xxx@xxx.xx>
2005-05-05 08:25:10 +0000

    removed warnings

--- org.glite.wms.client/src/commands/wmp-job-output.cpp
+++ org.glite.wms.client/src/commands/wmp-job-output.cpp
@@ -16,20 +16,10 @@ using namespace glite::wms::wmproxyapi;
 *	main
 */
 int main (int argc,char **argv){
-
-	ConfigContext *cfs = NULL;
-	string *endpoint = NULL ;
-	string *del_id ;
-	string jdl_string = "";
 	try {
 		// reads the user options
 		Options opts (Options::JOBOUTPUT) ;
 		opts.readOptions(argc, (const char**)argv);
-/*
-		// init of the context
-		cfs = new ConfigContext("", *endpoint, GLITE_TRUSTED_CERTS);
-		jobid = jobSubmit ( jdl_string , *del_id, cfs);
-*/
 
 	} catch (exception &ex) {
 		cout << flush << ex.what() << "\n" ;
