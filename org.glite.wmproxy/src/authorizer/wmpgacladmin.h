61088a498bda89825cf9db7af6cf2eedcf9e73f7
Giuseppe Avellino <xxx@xxx.xx>
2005-09-27 09:56:21 +0000

    gacl admin

---
+++ org.glite.wmproxy/src/authorizer/wmpgacladmin.h
@@ -0,0 +1,15 @@
+#ifndef GLITE_WMS_WMPROXY_WMPGACLADMIN_H
+#define GLITE_WMS_WMPROXY_WMPGACLADMIN_H
+
+#include "wmpgaclmanager.h"
+
+
+extern "C" {
+#include <getopt.h>
+}
+
+#endif // GLITE_WMS_WMPROXY_WMPGACLADMIN_H
+
+
+
+
