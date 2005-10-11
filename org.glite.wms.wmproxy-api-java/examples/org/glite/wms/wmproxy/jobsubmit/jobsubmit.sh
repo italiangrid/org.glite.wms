a87969bff06e69fcc1f8960326935c3867fc2aef
Marco Sottilaro <xxx@xxx.xx>
2005-10-11 08:12:31 +0000

    jobsubmit.sh

--- org.glite.wms.wmproxy-api-java/examples/org/glite/wms/wmproxy/jobsubmit/jobsubmit.sh
+++ org.glite.wms.wmproxy-api-java/examples/org/glite/wms/wmproxy/jobsubmit/jobsubmit.sh
@@ -25,19 +25,14 @@ echo "axis in : "$AXIS_LOC
 for p in \
 	${top_src} \
 	${top}stage/share/java/glite-wms-wmproxy-api-java.jar \
-	${top}repository/jclassads/2.1/share/classad.jar \
+	${top}repository/jclassads/2.2/share/classad.jar \
 	${top}stage/share/java/glite-wms-jdlj.jar \
 	${top}stage/share/java/glite-security-trustmanager.jar \
 	${top}stage/share/java/glite-security-util-java.jar \
 	${top}stage/share/java/glite-security-delegation-java.jar \
 	${top}repository/bcprov-jdk14/1.22/share/jars/bcprov-jdk14-122.jar \
 	 ${top}repository/bcprov-jdk14/1.22/share/jars/jce-jdk13-122.jar \
-	${top}repository/axis/1.2/share/axis-1.2/webapps/axis/WEB-INF/lib/axis.jar \
-	${top}repository/axis/1.2/share/axis-1.2/webapps/axis/WEB-INF/lib/jaxrpc.jar \
-	${top}repository/axis/1.2/share/axis-1.2/webapps/axis/WEB-INF/lib/log4j-1.2.8.jar \
-	${top}repository/axis/1.2/share/axis-1.2/webapps/axis/WEB-INF/lib/commons-logging.jar \
-	${top}repository/axis/1.2/share/axis-1.2/webapps/axis/WEB-INF/lib/commons-discovery.jar \
-	${top}repository/axis/1.2/share/axis-1.2/webapps/axis/WEB-INF/lib/saaj.jar
+	${AXIS_LOC}/*.jar 
 do
 	if ! printenv JSS_CLASSPATH | grep -q "${p}"; then
 		if [ -n "${classpath}" ]; then
