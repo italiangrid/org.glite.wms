b8924f5649ae2823cd3eae43d2f64cf9b1fc0699
Marco Cecchi <marco.cecchi@cnaf.infn.it>
2005-10-17 13:45:18 +0000

    gpbox utilities

---
+++ org.glite.wms.helper/src/broker/gpbox_utils.h
@@ -0,0 +1,20 @@
+#include <string>
+#include "glite/wmsutils/jobid/JobId.h"
+#include "glite/gpbox/Clientcc.h"
+#include "glite/wms/matchmaking/matchmaker.h"
+
+namespace jobid         = glite::wmsutils::jobid;
+namespace matchmaking   = glite::wms::matchmaking;
+
+namespace glite {
+namespace wms {
+namespace helper {
+namespace gpbox_utils {
+
+extern void dump_suitable_CEs(boost::scoped_ptr<matchmaking::match_table_t> const& suitable_CEs );
+extern std::string get_user_x509_proxy(jobid::JobId const& jobid);
+extern std::string get_proxy_distinguished_name(std::string const& proxy_file);
+extern bool filter_gpbox_authorizations(matchmaking::match_table_t& suitable_CEs,
+                                 Connection& PEP_connection,
+                                 std::string const& user_cert_file_name);
+}}}}
