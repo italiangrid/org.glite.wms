7d338bddf154b447e0f9f0e5fe109e61086a12b1
Alvise Dorigo <alvise.dorigo@pd.infn.it>
2005-11-04 12:45:44 +0000

    added exception for element not found in job cache

---
+++ org.glite.wms.ice/src/util/elementNotFound_ex.h
@@ -0,0 +1,27 @@
+
+#ifndef __ENF_EX_H__
+#define __ENF_EX_H__
+
+#include <exception>
+#include <string>
+
+namespace glite {
+  namespace wms {
+    namespace ice {
+      namespace util {
+
+	class elementNotFound_ex : public std::exception {
+	  
+	  std::string cause;
+	  
+	public:
+	  elementNotFound_ex(const std::string& _cause) throw() : cause(_cause) {};
+	  virtual ~elementNotFound_ex() throw() {}
+	  const char* what() const throw() { return cause.c_str(); }
+	};
+      }
+    }
+  }
+}
+
+#endif
