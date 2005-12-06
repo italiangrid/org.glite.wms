54bba9a94a8954225a4258599e56eea6dee32577
Alessandro Maraschini <alessandro.maraschini@telespazio.com>
2005-12-06 17:13:13 +0000

    debug print removed

--- org.glite.wms.client/src/utilities/adutils.cpp
+++ org.glite.wms.client/src/utilities/adutils.cpp
@@ -270,7 +270,6 @@ void AdUtils::setDefaultValuesAd(glite::wms::jdl::Ad* jdl,
 				if( 	glite_wms_client_toLower(attrName)!=JDL::RANK &&
 					glite_wms_client_toLower(attrName)!=JDL::REQUIREMENTS){
 					if (!jdl->hasAttribute(attrs[i])){
-						cout << "AdUtils::setDefaultValuesAd setting possible value: " <<attrs[i]<< endl ;
 						jdl->setAttributeExpr (attrs[i],confAd.lookUp(attrs[i]));
 					}
 				}
