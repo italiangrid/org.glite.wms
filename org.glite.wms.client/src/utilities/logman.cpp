1e18a2fa2b7eb157397c5a1c9d3d61645b519183
Marco Sottilaro <xxx@xxx.xx>
2006-01-19 16:14:38 +0000

    *** empty log message ***

--- org.glite.wms.client/src/utilities/logman.cpp
+++ org.glite.wms.client/src/utilities/logman.cpp
@@ -108,7 +108,7 @@ void Log::service(const std::string& service, const std::vector <std::pair<std::
 	int size = params.size( );
 	string msg = "Calling the WMProxy " + service  ;
 	if (size>0) {
-		msg += " service with the following parameters:\n";
+		msg += " service with the following parameter(s):\n";
 		for(int i=0; i < size; i++) {
 			msg += "> " + params[i].first + "=[" + params[i].second + "]\n";
 		}
