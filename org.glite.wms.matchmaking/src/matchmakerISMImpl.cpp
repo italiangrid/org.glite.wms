cd1d1bf5acba6dc0344481a841c2a4c998185751
Salvatore Monforte <salvatore.monforte@ct.infn.it>
2006-03-15 15:26:09 +0000

    Modification required to handle VOViews support in ISM

--- org.glite.wms.matchmaking/src/matchmakerISMImpl.cpp
+++ org.glite.wms.matchmaking/src/matchmakerISMImpl.cpp
@@ -6,34 +6,29 @@
 // $Id$
 
 #include <boost/shared_ptr.hpp>
-#include "glite/wms/common/logger/logger_utils.h"
+#include <algorithm>
 #include "glite/wms/common/utilities/classad_utils.h"
-
+#include "glite/wms/common/logger/logger_utils.h"
+#include "glite/wms/jdl/PrivateAdManipulation.h"
 #include "glite/wms/ism/ism.h"
 #include "matchmakerISMImpl.h"
 #include "exceptions.h"
 
 namespace glite {
 namespace wms {
-  
-namespace utilities     = common::utilities;
 
-namespace matchmaking { 
+namespace utilities = common::utilities;
 
-void 
+namespace matchmaking {
+
+void
 matchmakerISMImpl::prefetchCEInfo(
-  const classad::ClassAd* requestAd,
+  classad::ClassAd const* requestAd,
   match_table_t& suitableCEs
-) 
+)
 {
 }    
 
-/**
- * Check requirements.
- * This method fills suitableCEs vector with CEs satisfying requirements as expressed in the requestAd.
- * @param requestAd
- * @param suitableCEs
- */
 void
 matchmakerISMImpl::checkRequirement(
   classad::ClassAd& jdl,
@@ -51,11 +46,14 @@ matchmakerISMImpl::checkRequirement(
       continue;
     }
 
-    std::string const ce_id(ism_it->first);
     boost::shared_ptr<classad::ClassAd> ce_ad_ptr(
       boost::tuples::get<2>(ism_it->second)
     );
     classad::ClassAd ce_ad(*ce_ad_ptr);
+
+    std::string const ce_id(
+      utilities::evaluate_attribute(ce_ad, "GlueCEUniqueID")
+    );
 
     if (utilities::symmetric_match(ce_ad, jdl)) {
       Info(ce_id << ": ok!");
@@ -63,33 +61,29 @@ matchmakerISMImpl::checkRequirement(
     }
   }
 
-  typedef std::set<std::string> previous_matches_type;
+  typedef std::vector<std::string> previous_matches_type;
   previous_matches_type previous_matches;
+  jdl::get_edg_previous_matches(jdl, previous_matches);
+  previous_matches_type::iterator const previous_matches_begin(
+    previous_matches.begin()
+  );
   previous_matches_type::iterator const previous_matches_end(
     previous_matches.end()
   );
-  std::vector<match_table_t::iterator> v;
   match_table_t::iterator ces_it = suitableCEs.begin();
   match_table_t::iterator const ces_end = suitableCEs.end();
-  for ( ; ces_it != ces_end; ++ces_it) {
-    std::string const ce_id = ces_it->first;
-    if (previous_matches.find(ce_id) == previous_matches_end) {
-      v.push_back(ces_it);
+  while( ces_it != ces_end ) {
+    std::string const& ce_id = ces_it->first;
+    if (find(previous_matches_begin,
+             previous_matches_end, ce_id) != previous_matches_end) {
+      suitableCEs.erase(ces_it++);
     }
-  }
-  if (v.size() != suitableCEs.size()) {
-    std::vector<match_table_t::iterator>::iterator v_it = v.begin();
-    std::vector<match_table_t::iterator>::iterator const v_end = v.end();
-    for ( ; v_it != v_end; ++v_it) {
-      suitableCEs.erase(*v_it);
+    else {
+      ++ces_it;
     }
   }
 }
 
-/**
- * Checks the rank of CE in suitableCEs vector.
- * @param context a pointer to the matchmaking context.
- */
 void
 matchmakerISMImpl::checkRank(
   classad::ClassAd& jdl,
@@ -119,6 +113,4 @@ matchmakerISMImpl::checkRank(
   }
 }
 
-} // namespace matchmaking
-} // namespace wms
-} // namespace glite
+}}}
