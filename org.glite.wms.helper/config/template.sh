dc4414e151aa76b167251d68a62eaa2d4abac1fe
Marco Cecchi <marco.cecchi@cnaf.infn.it>
2006-05-31 08:03:56 +0000

    unneeded subshell removed

--- org.glite.wms.helper/config/template.sh
+++ org.glite.wms.helper/config/template.sh
@@ -528,61 +528,56 @@ if [ ${__job_type} -ne 3 ]; then #all but interactive
   fi
 fi
 
-(
-  perl -e '
-    unless (defined($ENV{"EDG_WL_NOSETPGRP"})) {
-      $SIG{"TTIN"} = "IGNORE";
-      $SIG{"TTOU"} = "IGNORE";
-      setpgrp(0, 0);
-    }
-    exec(@ARGV);
-    warn "could not exec $ARGV[0]: $!\n";
-    exit(127);
-  ' "$cmd_line" &
-  user_job=$!
+perl -e '
+  unless (defined($ENV{"EDG_WL_NOSETPGRP"})) {
+    $SIG{"TTIN"} = "IGNORE";
+    $SIG{"TTOU"} = "IGNORE";
+    setpgrp(0, 0);
+  }
+  exec(@ARGV);
+  warn "could not exec $ARGV[0]: $!\n";
+  exit(127);
+' "$cmd_line" &
+user_job=$!
 
-  exec 2> /dev/null
+exec 2> /dev/null
 
-  perl -e '
-    while (1) {
-      $time_left = `grid-proxy-info -timeleft 2> /dev/null` || 0;
-      last if ($time_left <= 0);
-      sleep($time_left);
-    }
-    kill(defined($ENV{"EDG_WL_NOSETPGRP"}) ? 9 : -9, '"$user_job"');
-    my $maradona = "'$maradona'";
-    my $logger = "'$lb_logevent'";
-    print STDERR $err_msg;
-    if (open(DAT,">> ".$maradona)) {
-      print DAT $err_msg;
-      close(DAT);
-    }
-    if (open(CMD, $logger.
-                  " --jobid=\"".$ENV{GLITE_WMS_JOBID}."\"".
-                  " --source=LRMS".
-                  " --sequence=".$ENV{GLITE_WMS_SEQUENCE_CODE}.
-                  " --event=\"Done\"".
-                  " --reason=\"Job killed because of user proxy expiration\"".
-                  " --status_code=FAILED".
-                  " --exit_code=0 2>/dev/null |")) {
-      chomp(my $value = <CMD>);
-      close(CMD);
-      my $result = $?;
-      my $exit_value = $result >> 8;
-      $ENV{GLITE_WMS_SEQUENCE_CODE} = $value if ($exit_value == 0);
-    }
-    exit(1);
-  ' &
-  watchdog=$!
-  wait $user_job
-  status=$?
-  #according to what reported (David McBride), the bash kill command
-  #doesn't appear to work properly on process groups
-  /bin/kill -9 $watchdog $user_job -$user_job
-  exit $status
-)
+perl -e '
+  while (1) {
+    $time_left = `grid-proxy-info -timeleft 2> /dev/null` || 0;
+    last if ($time_left <= 0);
+    sleep($time_left);
+  }
+  kill(defined($ENV{"EDG_WL_NOSETPGRP"}) ? 9 : -9, '"$user_job"');
+  my $maradona = "'$maradona'";
+  my $logger = "'$lb_logevent'";
+  print STDERR $err_msg;
+  if (open(DAT,">> ".$maradona)) {
+    print DAT $err_msg;
+    close(DAT);
+  }
+  if (open(CMD, $logger.
+                " --jobid=\"".$ENV{GLITE_WMS_JOBID}."\"".
+                " --source=LRMS".
+                " --sequence=".$ENV{GLITE_WMS_SEQUENCE_CODE}.
+                " --event=\"Done\"".
+                " --reason=\"Job killed because of user proxy expiration\"".
+                " --status_code=FAILED".
+                " --exit_code=0 2>/dev/null |")) {
+    chomp(my $value = <CMD>);
+    close(CMD);
+    my $result = $?;
+    my $exit_value = $result >> 8;
+    $ENV{GLITE_WMS_SEQUENCE_CODE} = $value if ($exit_value == 0);
+  }
+  exit(1);
+' &
 
+watchdog=$!
+wait $user_job
 status=$?
+#the bash kill command doesn't appear to work properly on process groups
+/bin/kill -9 $watchdog $user_job -$user_job
 
 #customization point #2
 if [ -n "${GLITE_LOCAL_CUSTOMIZATION_DIR}" ]; then
