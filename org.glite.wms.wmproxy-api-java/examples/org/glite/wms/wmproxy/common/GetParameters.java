f6e7067bee8300b9607ce5bd8374ce8ca8fada23
Marco Sottilaro <xxx@xxx.xx>
2006-09-27 09:17:37 +0000

    protocols and delegationId's

--- org.glite.wms.wmproxy-api-java/examples/org/glite/wms/wmproxy/common/GetParameters.java
+++ org.glite.wms.wmproxy-api-java/examples/org/glite/wms/wmproxy/common/GetParameters.java
@@ -1,6 +1,6 @@
 package org.glite.wms.wmproxy.common ;
 
-//import java.util.Vector ;
+import org.glite.wms.wmproxy.WMProxyAPI;
 
 import org.glite.wms.wmproxy.JobType;
 import org.glite.wms.wmproxy.JobTypeList;
@@ -8,7 +8,7 @@ import org.glite.wms.wmproxy.StringList;
 
 import java.io.BufferedReader;
 import java.io.FileReader ;
-
+import java.io.InputStreamReader ;
 
 import java.util.Hashtable;
 import java.lang.Integer ;
@@ -38,7 +38,175 @@ public class GetParameters {
 		this.typeTable.put (this._CHECKPOINTABLE,  JobType.CHECKPOINTABLE);
 
 	}
+	/**
+	* Returns the chosen protocol by user among the available ones
+	* or empty string (it means: all protocols)
+	*/
+	 public static String askForProtocol (WMProxyAPI client) {
+		boolean retrieve = false;
+		InputStreamReader isr = null ;
+		BufferedReader stdin = null ;
+		String input = "";
+		String numbers = "";
+		String protocol = "";
+		Integer conv = null;
+		String[] protocols = null;
+		StringList protoList = null ;
+		String[]  list = null;
+		int value = 0;
+		int size = 0;
+		// question to users
+		String question = "\nDo you want to retrieve the list of WMProxy available protocols [y|n] ?";
+		System.out.println (question);
+		System.out.println ("y = contacting the server to retrieve the list");
+		System.out.println ("n = retrieve the list of DestinationURI for all available protocols");
+		isr = new InputStreamReader( System.in );
+		stdin = new BufferedReader( isr );
+		while (true) {
+			try {
+				input = stdin.readLine();
+			} catch (java.io.IOException exc){
+				continue;
+			}
+			input = input.trim();
+			if (input.equals("y")) {
+				retrieve = true;
+				break;
+			} else if (input.equals("n")) {
+				retrieve = false;
+				break;
+			} else {
+				System.out.println (question);
+			}
+		}
+		if (retrieve) {
+			// Retrieving the protocol list ....
+			System.out.println( "Getting the list of available protocols from the endpoint " + client.getEndPoint());
+			try {
+				protoList = client.getTransferProtocols( );
+				if (protoList == null) {
+					System.out.println("You have received an empty list (Null Pointer received)");
+					protocol = DEFAULT_PROTOCOL_VALUE;
+				}
 
+			} catch (Exception exc) {
+				System.out.println ("\nWARNING Unable to retrieve the list of protocols; Retrieving all available protocols");
+				System.out.println ("exception caught (" + exc.getClass().getName() + ") - " + exc.getMessage( ) + "\n");
+				protocol = ALL_PROTOCOLS;
+			}
+			if (protoList != null) {
+				list = (String[])protoList.getItem( );
+				size = list.length ;
+				if (size==0) {
+					// The WMProxy doesn't have the list
+					System.out.println("You have received an empty list.");
+				} else if (size==1) {
+					// list with only one element
+					protocol = list[0];
+					System.out.println("The only available protocol is: " + protocol);
+				} else {
+					// choosing a protocol .....
+					System.out.println( "List of WMProxy available protocols:" );
+					System.out.println ("===========================================");
+					for (int i = 0; i < size; i++) {
+						System.out.println ("" + (i+1) + " - " + list[i]);
+						if ( i>0 && i<size){ numbers += " | ";}
+						numbers += "" + (i+1);
+					}
+					System.out.println ("a - all protocols in the list above");
+					System.out.println ("d - WMProxy default protocol");
+					while (true) {
+						System.out.println ("---------------------------");
+						System.out.println ("Choose one protocol in the list [" + numbers + "]");
+						System.out.println ("a = all protocols");
+						System.out.println ("d = default protocol");
+						try {
+							input = stdin.readLine();
+						} catch (java.io.IOException exc){
+							continue;
+						}
+						// removes white spaces (if there is any)
+						input = input.trim();
+						if (input.equals("a") ) {
+							protocol = ALL_PROTOCOLS;
+							break;
+						} else if (input.equals("d") ) {
+							protocol = DEFAULT_PROTOCOL_VALUE;
+							break;
+						}
+						else {
+							try {
+								conv = new Integer(input.trim());
+								value = conv.intValue();
+								if (value > 1 || value < size){
+									protocol = list[value-1];
+									break;
+								}
+							} catch ( NumberFormatException exc){
+								// invalid value: repeats this loop
+							}
+						}
+					}
+				}
+			}
+
+		} else {
+			// all protocols
+			protocol = ALL_PROTOCOLS;
+		}
+		return protocol;
+    	}
+	/**
+	* Returns the chosen delegationId
+	* or empty string (it means: default identifier)
+	*/
+	 public static String askForDelegationId( ) {
+		boolean retrieve = false;
+		InputStreamReader isr = null ;
+		BufferedReader stdin = null ;
+		String input = "";
+		String id = "";
+		// question to users
+		String question = "\nDo you want to type a delegationId [y|n] ?";
+		System.out.println (question);
+		System.out.println ("y = type your delegationID");
+		System.out.println ("n = use the default delegationID");
+		isr = new InputStreamReader( System.in );
+		stdin = new BufferedReader( isr );
+		while (true) {
+			try {
+				input = stdin.readLine();
+			} catch (java.io.IOException exc){
+				continue;
+			}
+			input = input.trim();
+			if (input.equals("y")) {
+				retrieve = true;
+				break;
+			} else if (input.equals("n")) {
+				retrieve = false;
+				break;
+			} else {
+				System.out.println (question);
+			}
+		}
+		if (retrieve) {
+			do {
+				System.out.print ("Type your delegationID: ");
+				try {
+					id = stdin.readLine();
+				} catch (java.io.IOException exc){
+					continue;
+				}
+			} while (id.length() == 0);
+		}
+		return id;
+	}
+	 /**
+	 * 	Protocol constants
+	 */
+	public static final String ALL_PROTOCOLS	= "all";
+	public static final String DEFAULT_PROTOCOL_VALUE = "";
 	// mapping of the JobType attributes
 	public static final String _NORMAL 			=  JobType._NORMAL ;
 	public static final String _PARAMETRIC 		= JobType._PARAMETRIC ;
