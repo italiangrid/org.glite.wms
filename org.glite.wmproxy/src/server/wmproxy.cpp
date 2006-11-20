04af166f0a18f15c983feff043a7f40286fb1e51
Giuseppe Avellino <xxx@xxx.xx>
2006-11-20 14:41:25 +0000

    Exception, Standard exception separation

--- org.glite.wmproxy/src/server/wmproxy.cpp
+++ org.glite.wmproxy/src/server/wmproxy.cpp
@@ -1,8 +1,21 @@
 /*
-	Copyright (c) Members of the EGEE Collaboration. 2004.
-	See http://public.eu-egee.org/partners/ for details on the copyright holders.
-	For license conditions see the license file or http://www.eu-egee.org/license.html
+Copyright (c) Members of the EGEE Collaboration. 2004. 
+See http://www.eu-egee.org/partners/ for details on the copyright
+holders.  
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+    http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
 */
+
 //
 // File: wmproxy.cpp
 // Author: Giuseppe Avellino <giuseppe.avellino@datamat.it>
@@ -156,14 +169,19 @@ main(int argc, char* argv[])
     	string msg = "Cannot open file: " + string(file.what());
 	    edglog(fatal)<<msg<<endl;
 	    WMProxy proxy;
-	   	sendFault(proxy, "main", msg, -4);
+	   	sendFault(proxy, "main", msg, -5);
     } catch (configuration::CannotConfigure &error) {
     	string msg = "Cannot configure: " + string(error.what());
 	    edglog(fatal)<<msg<<endl;
         WMProxy proxy;
+	   	sendFault(proxy, "main", msg, -4);
+	} catch (Exception &exc) {
+		string msg = "Exception caught: " + string(exc.what());
+	    edglog(fatal)<<msg<<endl;
+		WMProxy proxy;
 	   	sendFault(proxy, "main", msg, -3);
 	} catch (exception &ex) {
-		string msg = "Exception caught: " + string(ex.what());
+		string msg = "Standard Exception caught: " + string(ex.what());
 	    edglog(fatal)<<msg<<endl;
 		WMProxy proxy;
 	   	sendFault(proxy, "main", msg, -2);
