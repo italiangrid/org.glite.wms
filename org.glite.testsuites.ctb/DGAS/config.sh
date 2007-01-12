60e82a75fce2b3a6c44356ed40e91fcb4ab8dabc
Alessio Gianelle <alessio.gianelle@pd.infn.it>
2007-01-12 10:22:10 +0000

    Fix some minor bugs

--- org.glite.testsuites.ctb/DGAS/config.sh
+++ org.glite.testsuites.ctb/DGAS/config.sh
@@ -1,3 +1,7 @@
+# The "installation" directory of glite software
 GLITE_LOCATION=/home/ale/branch_3_0_0/stage/
-HOSTCERT=/etc/grid-security/hostcert.pem
+# The hostname and the port of the HRL to test
+HLR_HOSTNAME=wmstest-ce07.cr.cnaf.infn.it
 HLR_PORT=56568
+# The host certificate file on the HLR
+HOSTCERT=/etc/grid-security/hostcert.pem
