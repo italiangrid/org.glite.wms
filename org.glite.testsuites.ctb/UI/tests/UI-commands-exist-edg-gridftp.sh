8354950531ed9d1ebae3829d53bdcbdea8f0f05d
zaborov <zaborov>
2007-01-25 12:08:27 +0000

    check if gLite FTS commands can be found

---
+++ org.glite.testsuites.ctb/UI/tests/UI-commands-exist-edg-gridftp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Test whether the EDG GRID FTP commands can be found
+
+# Author: Dmitry Zaborov <Dmitry.Zaborov@cern.ch>
+# Version info: $Id$
+# Release: $Name$
+
+echo "    === Existence test of edg-gridftp-* ===    "
+
+. $(dirname $0)/command-exist.sh
+
+command_exist edg-gridftp-exists || exit $?
+command_exist edg-gridftp-ls     || exit $?
+command_exist edg-gridftp-mkdir  || exit $?
+command_exist edg-gridftp-rename || exit $?
+command_exist edg-gridftp-rm     || exit $?
+command_exist edg-gridftp-rmdir  || exit $?
+command_exist globus-url-copy    || exit $?
+
+echo "    ===  Ok  ===    "
