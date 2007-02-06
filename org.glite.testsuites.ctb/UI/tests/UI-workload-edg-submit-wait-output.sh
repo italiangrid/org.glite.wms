4a18f966ff49e3efb87264798bfa35b9796ab00f
zaborov <zaborov>
2007-02-06 14:13:29 +0000

    EDG workload CLI test

---
+++ org.glite.testsuites.ctb/UI/tests/UI-workload-edg-submit-wait-output.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# The submit - status - get output test for EDG workload system
+
+# Author: Dmitry Zaborov <Dmitry.Zaborov@cern.ch>
+# Version: $Id$
+
+$(dirname $0)/UI-workload-glite-submit-wait-output.sh -t EDG
