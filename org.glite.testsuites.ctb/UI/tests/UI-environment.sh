573ad7d2ab940193d7f2e17f7777c2d8ea8507c9
zaborov <zaborov>
2007-02-07 15:19:28 +0000

    a rewrite of the environment test

--- org.glite.testsuites.ctb/UI/tests/UI-environment.sh
+++ org.glite.testsuites.ctb/UI/tests/UI-environment.sh
@@ -1,52 +1,80 @@
-#!/bin/sh
+#!/bin/bash
 
-# This test helps to reveal common problems with shell environment,
+# This test helps to reveal common problems with environment variables,
 # such as wrong entries in $PATH and "similar" variables.
 # A failure is returned if a "single-entry" variable pointing to non-existing directory is found.
 # For colon-separated lists, such as $PATH, only a warning will be shown.
-
+#
+# Usage: 
+# UI-environment.sh - test environment seen from a bash script (~/.bashrc is executed)
+# or
+# source UI-environment.sh - test current shell environment (from bash only)
+#
 # Author: Dmitry Zaborov <Dmitry.Zaborov@cern.ch>
 # Version info: $Id$
-# Release: $Name$
 
+echo " === bash environment test === "
 
-echo " === general shell environment test === "
+# ... Part I: colon-separated lists
 
-function testpath() {
-  echo " --- testing $1 --- "
-  if [ -n "$2" ]; then
-    if [ -d "$2" ]; then
-    echo "info   : $1 is set to $2"
+for VAR_NAME_TEST in PATH LD_LIBRARY_PATH PYTHONPATH PERLLIB PERL5LIB MANPATH ; do
+
+  echo " --- testing $VAR_NAME_TEST --- "
+
+  # ... note that printenv is executed in a subshell, so variables not marked for export will not be seen
+  # ... and this is actually what we want since external (gLite) commands will not see such variables either
+
+  for DIR_NAME_TO_TEST in `printenv $VAR_NAME_TEST | sed 's/:/\n/g' | sort` ; do
+
+    if [ -d "$DIR_NAME_TO_TEST" ]; then
+      echo "info   : $VAR_NAME_TEST has $DIR_NAME_TEST"
     else
-      echo "ERROR  : $1 is set to non-existing location $2"
-      echo " === test NOT passed === "
-      exit 1
+      echo "WARNING: $VAR_NAME_TEST lists a non-existing location $DIR_NAME_TEST"
     fi
-  else
-    echo "info    : $1 is not defined"
-  fi
-}
 
-function testmultipath() {
-  echo " --- testing $1 --- "
-  for DIR in `echo $2 | sed 's/:/\n/g' | sort` ; do
-    if [ -d "$DIR" ]; then
-      echo "info   : $1 has $DIR"
-    else
-      echo "WARNING: $1 lists a non-existing location $DIR"
-    fi
   done
-}
 
-testmultipath PATH $PATH
-testmultipath LD_LIBRARY_PATH $LD_LIBRARY_PATH
-testmultipath PYTHONPATH $PYTHONPATH
-testmultipath PERLLIB $PERLLIB
-testmultipath PERL5LIB $PERL5LIB
-testmultipath MANPATH $MANPATH
+done
 
-testpath JAVA_HOME $JAVA_HOME
-testpath JAVA_INSTALL_PATH $JAVA_INSTALL_PATH
 
-echo " === test PASSED (but check warnings!) === "
-exit 0
+# .. error flag
+
+ENVIRONMENT_ERRORS=0
+
+# ... Part II: simple directory names
+
+for VAR_NAME_TEST in JAVA_HOME JAVA_INSTALL_PATH ; do
+
+  echo " --- testing $VAR_NAME_TEST --- "
+
+  DIR_NAME_TEST=`printenv $VAR_NAME_TEST`
+
+  if [ -z "$DIR_NAME_TEST" ]; then
+    echo "info    : $VAR_NAME_TEST is not defined or empty"
+  elif [ -d "$DIR_NAME_TEST" ]; then
+    echo "info   : $VAR_NAME_TEST is set to $DIR_NAME_TEST"
+  else
+    echo "ERROR  : $VAR_NAME_TEST is set to non-existing location $DIR_NAME_TEST"
+    ENVIRONMENT_ERRORS=1
+    break
+  fi
+
+done
+
+
+# ... just a little clean up (makes sence when this script is sourced)
+
+unset DIR_NAME_TEST
+unset VAR_NAME_TEST
+
+
+# ... the following enables return codes while allowing to "source" this file
+# ... (the use of "exit" would terminate the parent shell)
+
+if [ $ENVIRONMENT_ERRORS -ne 0 ]; then
+  echo " === test NOT passed === "
+  false
+else
+  echo " === test PASSED (but check warnings!) === "
+  true
+fi
