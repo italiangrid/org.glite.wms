6bff6a1f320ec037807606b8196a3c2f49ae6801
zaborov <zaborov>
2007-02-07 16:30:10 +0000

    edg-job-list-match test

---
+++ org.glite.testsuites.ctb/UI/tests/UI-workload-edg-job-list-match.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# A job-list-match test for the EDG submissoin system.
+
+# Author: Dmitry Zaborov <Dmitry.Zaborov@cern.ch>
+# Version: $Id$
+
+echo ""
+echo "    === test of edg-job-list-match ===    "
+echo ""
+
+JDLFILE=$(dirname $0)/hostname.jdl
+
+echo "--- will use JDL file as follows --- "
+echo ""
+cat $JDLFILE
+echo "--- end of JDL file --- "
+echo ""
+
+echo " # edg-job-list-match test # trying edg-job-list-match"
+echo ""
+
+edg-job-list-match --rank $JDLFILE
+
+if [ $? -ne 0 ]; then
+
+  echo " # edg-job-list-match test # ERROR: job-list-match failed"
+
+  echo ""
+  echo " *** something went wrong *** "
+  echo " *** test NOT passed *** "
+  exit 1
+
+fi
+
+echo "    === test PASSED === "
+exit 0
