318a3fab39868bb9f91412b40ef15f7b92c2e1bf
Marco Cecchi <marco.cecchi@cnaf.infn.it>
2007-02-15 09:03:54 +0000

    LB helper classes

--- org.glite.wms.common/src/utilities/lb_proxy.cpp
+++ org.glite.wms.common/src/utilities/lb_proxy.cpp
@@ -6,6 +6,8 @@
 
 #include "lb_proxy.h"
 #include <boost/bind.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <boost/tuple/tuple.hpp>
 #include <openssl/pem.h>
 #include <openssl/x509.h>
 
@@ -48,6 +50,65 @@ void sleep_while(unsigned int seconds, boost::function<bool()> condition)
   for (unsigned int i = 0; i < seconds && condition(); ++i) {
     ::sleep(1);
   }
+}
+
+boost::tuple<int,std::string,std::string>
+get_error_info(Context const& context)
+{
+  int error;
+  std::string error_txt;
+  std::string description_txt;
+
+  char* c_error_txt = 0;
+  char* c_description_txt = 0;
+  error = edg_wll_Error(context.c_context(), &c_error_txt, &c_description_txt);
+
+  if (c_error_txt) {
+    error_txt = c_error_txt;
+  }
+  free(c_error_txt);
+  if (c_description_txt) {
+    description_txt = c_description_txt;
+  }
+  free(c_description_txt);
+
+  return boost::make_tuple(error, error_txt, description_txt);
+}
+
+std::string
+get_lb_message(Context const& context)
+{
+  std::string result;
+
+  int error;
+  std::string error_txt;
+  std::string description_txt;
+  boost::tie(error, error_txt, description_txt) = get_error_info(context);
+
+  result += error_txt;
+  result += " (";
+  result += boost::lexical_cast<std::string>(error);
+  result += ") - ";
+  result += description_txt;
+
+  return result;
+}
+
+std::string
+format_log_message(std::string const& function_name, Context const& context)
+{
+  std::string result(function_name);
+  result += " failed for ";
+
+  edg_wlc_JobId c_jobid;
+  int e = edg_wll_GetLoggingJob(context.c_context(), &c_jobid);
+  assert(e == 0);
+  wmsutils::jobid::JobId jobid(c_jobid);
+  edg_wlc_JobIdFree(c_jobid);
+
+  result += jobid.toString() + " (" + get_lb_message(context) + ')';
+
+  return result;
 }
 
 } // anonymous
