7ab00d5c8b6b0b0c5fb390d177653baa359bbbf4
Marco Cecchi <marco.cecchi@cnaf.infn.it>
2007-02-21 10:18:21 +0000

    lb libs added (new dependency carried by wms.common)

--- org.glite.wms.helper/src/broker/gpbox_utils.h
+++ org.glite.wms.helper/src/broker/gpbox_utils.h
@@ -37,14 +37,14 @@ interact(
   configuration::Configuration const& config,
   jobid::JobId const& jobid,
   std::string const& PBOX_host_name,
-  matchmaking::matchtable& suitable_CEs);
+  matchmaking::match_table_t& suitable_CEs);
 
 bool
 interact(
   configuration::Configuration const& config,
   std::string const& x509_user_proxy,
   std::string const& PBOX_host_name,
-  matchmaking::matchtable& suitable_CEs);
+  matchmaking::match_table_t& suitable_CEs);
 
 }}}}}
 #endif
