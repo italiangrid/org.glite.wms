d9234a860ccd1c08e63cb1d2cae662a7986c43fb
zaborov <zaborov>
2007-02-22 16:52:14 +0000

    both -userconf and -confile are now tested one after another

--- org.glite.testsuites.ctb/UI/tests/UI-security-voms-proxy-init-userconf.sh
+++ org.glite.testsuites.ctb/UI/tests/UI-security-voms-proxy-init-userconf.sh
@@ -1,13 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
-# This test ensures that voms-proxy-init really uses the file given with the -userconf or -confile option.
+# This test ensures that voms-proxy-init really uses the files given with the -userconf and -confile options.
 # Also tested are -voms and -out options.
-# Note that voms-proxy-init ignores the sytem-wide vomses file if a user-level one is found,
-# thus both the options can not be tested simultaneousely.
+# Note that voms-proxy-init 1.6.16 has a bug preventing VOMSes given with -confile to be used
+# if such a VO was not defined in the default voms file ${GLITE_LOCATION}/etc/vomses
 
 # Author: Dmitry Zaborov <Dmitry.Zaborov@cern.ch>
 # Version info: $Id$
-# Release: $Name$
 
 SYSTEM_VOMS_FILE=${GLITE_LOCATION}/etc/vomses
 USER_VOMS_FILE=${HOME}/.glite/vomses
@@ -49,15 +48,18 @@ TMPPROXY=/tmp/proxy_`id  -u`
 echo ""
 myecho "trying to guess user's VO ..."
 if voms-proxy-info -vo; then
-  echo "voms-proxy-info -vo succeded"
+  myecho "voms-proxy-info -vo succeded"
   USER_VO=`voms-proxy-info -vo`
 fi
 if [ -n "$USER_VO" ]; then
-  myecho "the user is from $USER_VO"
+  myecho "the user is from \"$USER_VO\""
 else
-  USER_VO="" # just a dummy text
-  myecho "user proxy not found or does not have VO extension - could not guess VO"
+  USER_VO="" # just a dummy text, all VOMS servers will be put in the test file
+  myecho "user proxy not found or does not have VO extension - will try all VOMSes"
 fi
+
+echo ""
+echo "    === Part I: -userconf === "
 
 if [ -f ${USER_VOMS_FILE} ]; then
 
@@ -69,19 +71,34 @@ if [ -f ${USER_VOMS_FILE} ]; then
   myecho "making a slightly modified test copy"
   awk -F ' ' '/[:print:]/ {$1="\"testvoms\""} {print $0}' $USER_VOMS_FILE | grep "$USER_VO" > $TMP_USER_VOMS_FILE
 
-  chmod 644 $TMP_USER_VOMS_FILE 
+  chmod 644 $TMP_USER_VOMS_FILE
   cat $TMP_USER_VOMS_FILE
 
   echo ""
   myecho "trying  voms-proxy-init -voms testvoms -userconf $TMP_USER_VOMS_FILE -out $TMPPROXY"
   voms-proxy-init -voms testvoms  -userconf $TMP_USER_VOMS_FILE  -out $TMPPROXY || myexit $?
 
+  echo ""
+  myecho "destroying proxy ..."
+  voms-proxy-destroy -file $TMPPROXY || myexit $?
+
+  echo "    --- Part I succeeded --- "
+
 elif [ -d ${USER_VOMS_FILE} ]; then
 
     myecho "the use directory of user-defined voms files is not supported by the current version of this test, sorry"
     myexit 1
 
-elif [ -e ${SYSTEM_VOMS_FILE} ]; then
+else
+
+  myecho "Warning: ${USER_VOMS_FILE} not found - have to skip this test"
+
+fi
+
+echo ""
+echo "    === Part II: -confile === "
+
+if [ -e ${SYSTEM_VOMS_FILE} ]; then
 
   if [ -f ${SYSTEM_VOMS_FILE} ]; then
 
@@ -95,7 +112,7 @@ elif [ -e ${SYSTEM_VOMS_FILE} ]; then
   elif [ -d ${SYSTEM_VOMS_FILE} ]; then
 
     myecho "${SYSTEM_VOMS_FILE} is a directory "
-    myecho " -> will try to merge its files into a test voms file"
+    myecho " -> will merge its files into a test voms file"
     echo ""
     myecho "making a test voms file"
     cat ${SYSTEM_VOMS_FILE}/* | grep "CN" | awk -F ' ' '/[:print:]/ {$1="\"testvoms\""} {print $0}' /dev/stdin | grep "$USER_VO" > ${TMP_SYSTEM_VOMS_FILE}
@@ -107,24 +124,16 @@ elif [ -e ${SYSTEM_VOMS_FILE} ]; then
     
   fi
   
-  chmod 644 $TMP_SYSTEM_VOMS_FILE 
+  chmod 644 $TMP_SYSTEM_VOMS_FILE
   cat $TMP_SYSTEM_VOMS_FILE
-
-  #echo "making an empty \"voms\" file"
-  #echo "" > $TMP_USER_VOMS_FILE
 
   echo ""
   myecho "trying voms-proxy-init -debug -voms testvoms -confile $TMP_SYSTEM_VOMS_FILE -out $TMPPROXY"
   voms-proxy-init -debug -voms testvoms  -confile $TMP_SYSTEM_VOMS_FILE  -out $TMPPROXY || myexit $?
 
-  chmod 644 $TMP_SYSTEM_VOMS_FILE 
-  cat $TMP_SYSTEM_VOMS_FILE
-  myexit 1
-
 else
 
-  myecho "neither ${USER_VOMS_FILE} nor ${SYSTEM_VOMS_FILE} exists - VOMS extension can not be tested"
-  myexit 1
+  myecho "WARNING: ${SYSTEM_VOMS_FILE} does not exists - VOMS extension can not be tested"
   
 fi
 
