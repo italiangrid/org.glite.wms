c81c94a3518461877f8ba3c86d00b53f563af67c
Francesco Giacomini <francesco.giacomini@cern.ch>
2007-03-09 12:01:43 +0000

    utilities to read generically from filelist or jobdir

---
+++ org.glite.wms.common/src/utilities/input_reader.cpp
@@ -0,0 +1,23 @@
+// File: input_reader.cpp
+// Author: Francesco Giacomini
+// Copyright (c) Members of the EGEE Collaboration 2004
+// For license conditions see http://public.eu-egee.org/license/license.html
+
+// $Id$
+
+#include "input_reader.h"
+
+namespace glite {
+namespace wms {
+namespace common {
+namespace utilities {
+
+InputItem::~InputItem()
+{
+}
+
+InputReader::~InputReader()
+{
+}
+
+}}}}
