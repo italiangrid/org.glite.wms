ccb8d118c75cbfe4b6230db7a4757438fe9f500e
klopov <klopov>
2007-03-18 16:25:02 +0000

    Wraper script for checkpointable job

--- org.glite.testsuites.ctb/WMS/tests/chkptmake.sh
+++ org.glite.testsuites.ctb/WMS/tests/chkptmake.sh
@@ -1,29 +1,28 @@
 #!/bin/bash
 
-echo "==================== Try to make C++ job"
-make -f makeWN 2>&1
-ret=$?
+if [ $1 = 1 ]; then 
+ echo "==================== Try to make C++ job"
+ make -f makeWN 2>&1
+ ret=$?
 
-if [ $ret != 0 ]; then
- echo "================== Warning: can not make C++ job. hostname: `hostname` :"
- echo "make exit with exit code= $ret"
+ if [ $ret != 0 ]; then
+  echo "================== Warning: can not make C++ job. hostname: `hostname` :"
+  echo "make exit with exit code= $ret"
+
+  echo "================== Run job from InputSandbox"
+  chmod 755 ./chkjob1
+  ./chkjob1
+ else
+  echo "make C++ job OK"
+  echo "================== Run job"
+  ./chkjob
+ fi
+else
  echo "================== Run job from InputSandbox"
  chmod 755 ./chkjob1
  ./chkjob1
-else
- echo "make C++ job OK"
- echo "================== Run job"
- ./chkjob
 fi
 
 exit 0
 
-#if [ -e chkjob ]; then
-# chmod 755 ./chkjob
-# ./chkjob
-# exit 0
-#else
-# echo "No file chkjob"
-# exit 1
-#fi
 
