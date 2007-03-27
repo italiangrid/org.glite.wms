b914c46ad905d7e5c2c0c295292f4db7971d6d9d
zaborov <zaborov>
2007-03-27 09:19:19 +0000

    delegate proxy, submit job, wait and get output

---
+++ org.glite.testsuites.ctb/UI/tests/UI-workload-glite-wms-deleg-submit-wait-output.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# "Delegate proxy - submit - get status - get output" test for gLite WMS workload system
+
+# Author: Dmitry Zaborov <Dmitry.Zaborov@cern.ch>
+# Version: $Id$
+
+$(dirname $0)/UI-workload-glite-submit-wait-output.sh -t gLite-WMS-d
