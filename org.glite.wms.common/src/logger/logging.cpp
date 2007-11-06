9b9ae029c90db6f13cb2e0dcdfff0930c9fb971c
Francesco Giacomini <francesco.giacomini@cern.ch>
2007-11-06 14:26:31 +0000

    added security and control logs

--- org.glite.wms.common/src/logger/logging.cpp
+++ org.glite.wms.common/src/logger/logging.cpp
@@ -14,20 +14,19 @@ namespace glite {
 //namespace common {
 namespace logging {
 
-NullLogger null_logger;
+NullLogger null_log;
 Logger access_log;
-
-// extern Logger security_log;
-// extern Logger control_log;
+Logger security_log;
+Logger control_log;
 
 int Init::s_count = 0;
 
 namespace {
 
-void init_logging()
+void init_access_logging()
 {
   log4cpp::SyslogAppender* appender(
-    new log4cpp::SyslogAppender("syslog", "log4cpp")
+    new log4cpp::SyslogAppender("syslog", "access")
   );
 //  log4cpp::FileAppender* appender(
 //    new log4cpp::FileAppender("file", "/home/giaco/tmp/log")
@@ -44,12 +43,54 @@ void init_logging()
   access_log.native_logger(&access);
 }
 
+void init_security_logging()
+{
+  log4cpp::SyslogAppender* appender(
+    new log4cpp::SyslogAppender("syslog", "security")
+  );
+//  log4cpp::FileAppender* appender(
+//    new log4cpp::FileAppender("file", "/home/giaco/tmp/log")
+//  );
+
+  appender->setLayout(new log4cpp::SimpleLayout());
+
+  log4cpp::Category& security(
+    log4cpp::Category::getInstance(std::string("security"))
+  );
+
+  security.addAppender(appender);
+  security.setPriority(log4cpp::Priority::INFO);
+  security_log.native_logger(&security);
+}
+
+void init_control_logging()
+{
+  log4cpp::SyslogAppender* appender(
+    new log4cpp::SyslogAppender("syslog", "control")
+  );
+//  log4cpp::FileAppender* appender(
+//    new log4cpp::FileAppender("file", "/home/giaco/tmp/log")
+//  );
+
+  appender->setLayout(new log4cpp::SimpleLayout());
+
+  log4cpp::Category& control(
+    log4cpp::Category::getInstance(std::string("control"))
+  );
+
+  control.addAppender(appender);
+  control.setPriority(log4cpp::Priority::INFO);
+  control_log.native_logger(&control);
+}
+
 }
 
 Init::Init()
 {
   if (s_count++ == 0) {
-    init_logging();
+    init_access_logging();
+    init_control_logging();
+    init_security_logging();
   }
 }
 
@@ -57,6 +98,8 @@ Init::~Init()
 {
   if (--s_count == 0) {
     access_log.native_logger(0);
+    security_log.native_logger(0);
+    control_log.native_logger(0);
   }
 }
 
