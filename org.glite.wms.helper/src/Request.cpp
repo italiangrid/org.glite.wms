4dc66d3b3edf407cf4dbf74ab5dfbb016246e563
Marco Cecchi <marco.cecchi@cnaf.infn.it>
2008-02-08 15:30:33 +0000

    .

--- org.glite.wms.helper/src/Request.cpp
+++ org.glite.wms.helper/src/Request.cpp
@@ -7,6 +7,7 @@
 
 #include "Request.h"
 #include <stdexcept>
+#include <boost/shared_ptr.hpp>
 #include <classad_distribution.h>
 #include "RequestStateMachine.h"
 
@@ -20,6 +21,7 @@ class Request::Impl: boost::noncopyable
 
   classad::ClassAd const* m_original_ad;
   classad::ClassAd* m_current_ad;
+  boost::shared_ptr<std::string> m_jw_template;
 
   void change_ad(classad::ClassAd* new_ad)
   {
@@ -28,20 +30,26 @@ class Request::Impl: boost::noncopyable
   }
 
 public:
-  explicit Impl(const classad::ClassAd* ad);
+  explicit Impl(
+    const classad::ClassAd* ad,
+    boost::shared_ptr<std::string> jw_template
+  );
   ~Impl();
 
   void resolve();
-  bool is_resolved() const { return ! m_state_machine; }
+  bool is_resolved() const { return !m_state_machine; }
 
   classad::ClassAd const* original_ad() const { return m_original_ad; }
   classad::ClassAd const* current_ad() const { return m_current_ad; }
 };
 
-Request::Impl::Impl(const classad::ClassAd* ad)
-  : m_state_machine(0),
+Request::Impl::Impl(
+  const classad::ClassAd* ad,
+  boost::shared_ptr<std::string> jw_template
+) : m_state_machine(0),
     m_original_ad(ad),
-    m_current_ad(new classad::ClassAd(*m_original_ad))
+    m_current_ad(new classad::ClassAd(*m_original_ad)),
+    m_jw_template(jw_template)
 {
   m_state_machine.start(m_original_ad);
 }
@@ -56,16 +64,21 @@ Request::Impl::resolve()
 try {
   assert(m_state_machine);
 
-  std::auto_ptr<classad::ClassAd> resolved_ad(m_state_machine.next_step(m_current_ad));
+  std::auto_ptr<classad::ClassAd> resolved_ad(
+    m_state_machine.next_step(m_current_ad, m_jw_template)
+  );
 
   change_ad(resolved_ad.release());
 } catch (helper::NoValidState const&) {
   throw std::invalid_argument("Invalid request");
 }
 
-Request::Request(const classad::ClassAd* ad)
+Request::Request(
+  const classad::ClassAd* ad,
+  boost::shared_ptr<std::string> jw_template
+)
 try
-  : m_impl(new Impl(ad))
+  : m_impl(new Impl(ad, jw_template))
 {
 } catch (NoValidState const&) {
   throw std::invalid_argument("invalid request");
