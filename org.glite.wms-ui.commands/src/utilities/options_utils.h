97ce187f7fdd622636d524583407aea5b409d1f0
Luca Petronzio <luca.petronzio@telespazio.com>
2008-03-12 11:22:21 +0000

    unused option removed

--- org.glite.wms-ui.commands/src/utilities/options_utils.h
+++ org.glite.wms-ui.commands/src/utilities/options_utils.h
@@ -93,7 +93,6 @@ class Options
 		} ;
                 enum TimeOpts{
                 	TIME_NO_OPT,
-			TIME_FROM,
                         TIME_TO,
                         TIME_VALID
                 } ;
