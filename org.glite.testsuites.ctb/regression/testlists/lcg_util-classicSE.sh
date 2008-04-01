708ded5ce7f24fbaae30f2d7380ab9ad38e3dd2e
unterkir <unterkir>
2008-04-01 13:35:18 +0000

    *** empty log message ***

---
+++ org.glite.testsuites.ctb/regression/testlists/lcg_util-classicSE.sh
@@ -0,0 +1,12 @@
+#VO=org.glite.voms-test
+VO=dteam
+DPM_SE=lxb1921.cern.ch
+CLASSIC_SE=lxb0976.cern.ch
+DEFAULT_SE=${DPM_SE}
+DEFAULT_LFC=lxb1941.cern.ch
+# Production Settings
+#VO=dteam
+#DPM_SE=lxdpm101.cern.ch
+#CLASSIC_SE=arxiloxos2.inp.demokritos.gr
+#DEFAULT_SE=${DPM_SE}
+#DEFAULT_LFC=prod-lfc-shared-central.cern.ch
