bfa3a8866dd4ee186e7c52a4a693297d36165020
Alvise Dorigo <alvise.dorigo@pd.infn.it>
2008-04-08 08:34:33 +0000

    updated VersionID

--- org.glite.wms.ice/src/glite-wms-ice.cpp
+++ org.glite.wms.ice/src/glite-wms-ice.cpp
@@ -239,7 +239,7 @@ int main(int argc, char*argv[])
 
     logger_instance->setLogFile(logfile.c_str());
     CREAM_SAFE_LOG(log_dev->debugStream() 
-		   << "ICE VersionID is [20080402-09:40] ProcessID=["
+		   << "ICE VersionID is [20080408-10:30] ProcessID=["
 		   << ::getpid() << "]"
 		   <<log4cpp::CategoryStream::ENDLINE);
     cout << "Logfile is [" << logfile << "]" << endl;
