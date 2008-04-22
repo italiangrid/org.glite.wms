af3adb3efeb76cb88c150047eefeaf82d0494413
unterkir <unterkir>
2008-04-22 14:45:26 +0000

    *** empty log message ***

---
+++ org.glite.testsuites.ctb/MPI/mpi/c/mpi-start-wrapper.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Pull in the arguments.
+MY_EXECUTABLE=`pwd`/$1
+MPI_FLAVOR=$2
+
+# Convert flavor to lowercase for passing to mpi-start.
+MPI_FLAVOR_LOWER=`echo $MPI_FLAVOR | tr '[:upper:]' '[:lower:]'`
+
+# Pull out the correct paths for the requested flavor.
+eval MPI_PATH=`printenv MPI_${MPI_FLAVOR}_PATH`
+
+# Ensure the prefix is correctly set.  Don't rely on the defaults.
+eval I2G_${MPI_FLAVOR}_PREFIX=$MPI_PATH
+export I2G_${MPI_FLAVOR}_PREFIX
+
+# Touch the executable.  It exist must for the shared file system check.
+# If it does not, then mpi-start may try to distribute the executable
+# when it shouldn't.
+touch $MY_EXECUTABLE
+
+# Setup for mpi-start.
+export I2G_MPI_APPLICATION=$MY_EXECUTABLE
+export I2G_MPI_APPLICATION_ARGS=
+export I2G_MPI_TYPE=$MPI_FLAVOR_LOWER
+export I2G_MPI_PRE_RUN_HOOK=mpi-hooks.sh
+export I2G_MPI_POST_RUN_HOOK=mpi-hooks.sh
+
+# If these are set then you will get more debugging information.
+export I2G_MPI_START_VERBOSE=1
+#export I2G_MPI_START_DEBUG=1
+
+# Invoke mpi-start.
+$I2G_MPI_START
