b8bbd6ac6859213eea352159534c975f541ee833
zaborov <zaborov>
2008-04-24 08:06:02 +0000

    remove checks for deprecated commands

--- org.glite.testsuites.ctb/UI/tests/UI-version-glite-job.sh
+++ org.glite.testsuites.ctb/UI/tests/UI-version-glite-job.sh
@@ -8,12 +8,8 @@
 echo "    === Version test of glite-job-*  ===     "
 echo ""
 
-source `dirname $0`/command-version.sh glite-job-submit       --version || exit $?
 source `dirname $0`/command-version.sh glite-job-status       --version || exit $?
-source `dirname $0`/command-version.sh glite-job-list-match   --version || exit $?
-source `dirname $0`/command-version.sh glite-job-cancel       --version || exit $?
 source `dirname $0`/command-version.sh glite-job-attach       --version || exit $?
-source `dirname $0`/command-version.sh glite-job-output       --version || exit $?
 source `dirname $0`/command-version.sh glite-job-logging-info --version || exit $?
 source `dirname $0`/command-version.sh glite-job-get-chkpt    --version || exit $?
 
