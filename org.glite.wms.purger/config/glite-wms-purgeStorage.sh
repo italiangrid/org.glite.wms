23a39e4cca6afd8b5743c89a8bc4a6cf0f0ecac6
Salvatore Monforte <salvatore.monforte@ct.infn.it>
2008-05-23 11:50:08 +0000

    Fix for bug #36902: Cron job to renew host-proxy

--- org.glite.wms.purger/config/glite-wms-purgeStorage.sh
+++ org.glite.wms.purger/config/glite-wms-purgeStorage.sh
@@ -1,36 +1,17 @@
 #!/bin/bash
-
-create_proxy()
-{
-   proxy=`"${GLOBUS_LOCATION}/bin/grid-proxy-init" -q \
-     -cert $GLITE_HOST_CERT \
-     -key  $GLITE_HOST_KEY \
-     -hours 24 \
-     -out  "$1.$$" 2>&1`
-   
-   [ $? -eq 0 ] || echo $proxy " Fail " >> $2
-
-   move=`chmod 400 "$1.$$" &&
-     chown "${GLITE_WMS_USER}" "$1.$$" &&
-     mv "$1.$$" "$1" 2>&1`
-
-   [ $? -eq 0 ] || echo $move " Fail " >> $2
-}
+source /etc/init.d/functions
 
 check_process()
 {
-  ps ax | grep -v grep | grep -v $1.sh | grep $1 > /dev/null
+  pid=`pidofproc glite-wms-purgeStorage`
   if [ $? -eq 0 ] ; then
-    echo "glite-wms-purgeStorage already running..." >> $2
+    echo "glite-wms-purgeStorage already running on pid " $pid  >> $2
     exit 1
   fi
 }
 
-#create_proxy  "${GLITE_LOCATION_VAR}/purger.proxy" $log
-#exit 1
-
 log="/dev/stdout"
-while getopts "l:t:c:p:a:bfeqh" arg
+while getopts "l:t:c:p:a:snoh" arg
 do
   case "$arg" in
   l)    log="$OPTARG";;
@@ -39,11 +20,7 @@ do
   esac
 done
 
-if [ ! "${GLITE_WMS_PROXY_FILENAME}" ] 
-then
-  GLITE_WMS_PROXY_FILENAME="/wms.proxy"
-fi
-create_proxy  "${GLITE_WMS_LOCATION_VAR}/${GLITE_WMS_PROXY_FILENAME}" $log
+create-proxy.sh
 check_process glite-wms-purgeStorage $log
 
 # if another instance had been running the check_process
