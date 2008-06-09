8d0b58be3e1f082f19c79f28269cbcdcccb93bcb
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2008-06-09 09:47:03 +0000

    New CREAM-cli-delegation-renew.sh

--- org.glite.testsuites.ctb/CREAM/CREAM-cli/CREAM-cli-delegation-renew.sh
+++ org.glite.testsuites.ctb/CREAM/CREAM-cli/CREAM-cli-delegation-renew.sh
@@ -17,29 +17,33 @@
 
 prepare $@
 
-my_echo "TEST 0: delegate a proxy specifying CREAM URL and delegation ID:"
+my_echo "TEST 0: renew a proxy specifying CREAM URL and delegation ID:"
 
 run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-delegate-proxy -e $ENDPOINT DelegateId_$$_0
+if [ $? -ne 0 ]; then
+  exit_failure ${COM_OUTPUT}
+fi
+
+run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-proxy-renew -e $ENDPOINT DelegateId_$$_0
 if [ $? -ne 0 ]; then
   exit_failure ${COM_OUTPUT}
 else
   success
 fi
 
-my_echo "TEST 1: re-delegate the proxy specified in TEST 0:"
-
-run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-delegate-proxy -e $ENDPOINT DelegateId_$$_0
-RESULT=`echo ${COM_OUTPUT} | grep "Delegation ID 'DelegateId_$$_0' already exists"`
+my_echo "TEST 1: try to renew a missing proxy:"
+run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-proxy-renew -e $ENDPOINT DelegateId_$$_1
+RESULT=`echo ${COM_OUTPUT} | grep "delegation ID was not delegated"`
 if [ -z "$RESULT" ]; then
   exit_failure ${COM_OUTPUT}
 else
   success
 fi
 
-my_echo "TEST 2: delegate a proxy and append the output to the existing file ${LOGFILE}:";
+my_echo "TEST 2: renew a proxy and append the output to the existing file ${LOGFILE}:";
 echo "#HEADER#" > ${LOGFILE} || exit_failure "Cannot open ${LOGFILE}";
-run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-delegate-proxy \
---logfile ${LOGFILE} -e $ENDPOINT DelegateId_$$_1
+run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-proxy-renew \
+--logfile ${LOGFILE} -e $ENDPOINT DelegateId_$$_0
 RESULT=`grep "#HEADER#" ${LOGFILE}`
 if [ -z "$RESULT" ]; then
   exit_failure "File ${LOGFILE} has been overwrite"
@@ -52,20 +56,20 @@ else
   fi
 fi
 
-my_echo "TEST 3: delegate a proxy specifying a client configuration file:";
-mkdir ${MYTMPDIR}/delegate_log_dir || exit_failure "Cannot create ${MYTMPDIR}/delegate_log_dir";
+my_echo "TEST 3: renew a proxy specifying a client configuration file:";
+mkdir ${MYTMPDIR}/renew_log_dir || exit_failure "Cannot create ${MYTMPDIR}/renew_log_dir";
 printf "[
-DELEGATE_LOG_DIR=\"${MYTMPDIR}/delegate_log_dir\";
+PROXYRENEW_LOG_DIR=\"${MYTMPDIR}/renew_log_dir\";
 CREAMDELEGATION_URL_PREFIX=\"https://\";
 CREAMDELEGATION_URL_POSTFIX=\"ce-cream/services/gridsite-delegation\";
 ]
 " > ${MYTMPDIR}/delegate.conf
-run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-delegate-proxy \
---debug --conf ${MYTMPDIR}/delegate.conf -e $ENDPOINT DelegateId_$$_2
+run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-proxy-renew \
+--debug --conf ${MYTMPDIR}/delegate.conf -e $ENDPOINT DelegateId_$$_0
 if [ $? -ne 0 ]; then
   exit_failure ${COM_OUTPUT}
 else
-  RESULT=`ls ${MYTMPDIR}/delegate_log_dir/* | grep glite-ce-delegate-proxy_CREAM | wc -l 2>/dev/null`
+  RESULT=`ls ${MYTMPDIR}/renew_log_dir/* | grep glite-ce-proxy-renew_CREAM | wc -l 2>/dev/null`
   if [ $RESULT == "0" ]; then
     exit_failure "Cannot find debug log file"
   else
@@ -73,6 +77,5 @@ else
   fi
 fi
 
+
 exit_success
-
-
