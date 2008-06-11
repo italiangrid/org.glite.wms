f54e20f8238593a61439fd80d44949a15cbb88be
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2008-06-11 11:41:39 +0000

    Fixed delegation test

--- org.glite.testsuites.ctb/CREAM/CREAM-cli/CREAM-cli-delegation.sh
+++ org.glite.testsuites.ctb/CREAM/CREAM-cli/CREAM-cli-delegation.sh
@@ -17,12 +17,15 @@
 
 prepare $@
 
+FAILED=0
+
 my_echo "TEST 0: delegate a proxy specifying CREAM URL and delegation ID:"
 
 PROXY_ID=`new_delegation_id`
 run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-delegate-proxy -e $ENDPOINT $PROXY_ID
 if [ $? -ne 0 ]; then
-  exit_failure ${COM_OUTPUT}
+  failure ${COM_OUTPUT}
+  ((FAILED++)) # continue
 else
   success
 fi
@@ -32,7 +35,8 @@ my_echo "TEST 1: re-delegate the proxy specified in TEST 0:"
 run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-delegate-proxy -e $ENDPOINT $PROXY_ID
 RESULT=`echo ${COM_OUTPUT} | grep "Delegation ID '$PROXY_ID' already exists"`
 if [ -z "$RESULT" ]; then
-  exit_failure ${COM_OUTPUT}
+  failure ${COM_OUTPUT}
+  ((FAILED++)) # continue
 else
   success
 fi
@@ -43,11 +47,13 @@ run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-delegate-proxy \
 --logfile ${LOGFILE} -e $ENDPOINT `new_delegation_id`
 RESULT=`grep "#HEADER#" ${LOGFILE}`
 if [ -z "$RESULT" ]; then
-  exit_failure "File ${LOGFILE} has been overwrite"
+  failure "File ${LOGFILE} has been overwrite"
+  ((FAILED++)) # continue
 else
-  RESULT=`grep -P "INFO|ERROR|WARN" ${LOGFILE}`
+  RESULT=`grep -P "INFO|ERROR|WARN|NOTICE" ${LOGFILE}`
   if [ -z "$RESULT" ]; then
-    exit_failure "Cannot log on file ${LOGFILE}"  
+    failure "Cannot log on file ${LOGFILE}"
+    ((FAILED++)) # continue
   else
     success
   fi
@@ -64,16 +70,21 @@ CREAMDELEGATION_URL_POSTFIX=\"ce-cream/services/gridsite-delegation\";
 run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-delegate-proxy \
 --debug --conf ${MYTMPDIR}/delegate.conf -e $ENDPOINT `new_delegation_id`
 if [ $? -ne 0 ]; then
-  exit_failure ${COM_OUTPUT}
+  failure ${COM_OUTPUT}
+  ((FAILED++)) # continue
 else
   RESULT=`ls ${MYTMPDIR}/delegate_log_dir/* | grep glite-ce-delegate-proxy_CREAM | wc -l 2>/dev/null`
   if [ $RESULT == "0" ]; then
-    exit_failure "Cannot find debug log file"
+    failure "Cannot find debug log file"
+    ((FAILED++)) # continue
   else
     success
   fi
 fi
 
-exit_success
-
+if [ $FAILED -gt 0 ] ; then
+  exit_failure "$FAILED test(s) failed on 4 differents tests"
+else
+  exit_success
+fi
 
