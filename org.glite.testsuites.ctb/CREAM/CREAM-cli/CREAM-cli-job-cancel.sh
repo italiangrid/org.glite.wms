8c32e768fa65a4a9eb3e7b09e27e679da5e12919
Alessio Gianelle <alessio.gianelle@pd.infn.it>
2008-06-12 13:54:03 +0000

    Add description

--- org.glite.testsuites.ctb/CREAM/CREAM-cli/CREAM-cli-job-cancel.sh
+++ org.glite.testsuites.ctb/CREAM/CREAM-cli/CREAM-cli-job-cancel.sh
@@ -2,9 +2,14 @@
 
 ###############################################################################
 #
-# A basic job interruption operation
+# Test glite-ce-job-cancel command:
 #
-# Features: This test performs several cancel calls on the server.
+# TEST 1: submit and then cancel a running job
+# TEST 2: try to cancel a terminated job
+# TEST 3: check if the --all option works
+# TEST 4: check the requirements of the --all option (3 cases)
+# TEST 5: check if the --conf option works
+# TEST 6: save info into a logfile to check if --debug and --logfile options work 
 #
 # Author: Paolo Andreetto <sa3-italia@mi.infn.it>
 # Version: $Id:
@@ -19,11 +24,40 @@ prepare $@
 
 FAILED=0
 
-my_echo "TEST 0: submit and then cancel a running job:"
+my_echo ""
+
+TESTCOMMAND="${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-job-cancel"
+
+if [ ! -x ${TESTCOMMAND} ] ; then
+  exit_failure "Command ${TESTCOMMAND} not exists, test could not be performed!"
+fi
+
+####
+
+my_echo "TEST 1: submit and then cancel a running job:"
 
 wait_until_job_runs
+
+run_command "${TESTCOMMAND} --noint $JOBID"
 if [ $? -ne 0 ]; then
-  failure ${COM_OUTPUT}
+  exit_failure "Command failed: ${COM_OUTPUT}"
+else
+  success
+fi
+
+####
+
+my_echo "TEST 2: try to cancel a terminated job:"
+
+wait_until_job_finishes
+
+run_command "${TESTCOMMAND} --noint $JOBID"
+if [ $? -ne 0 ]; then
+  exit_failure "Command failed: ${COM_OUTPUT}"
+fi
+RESULT=`echo ${COM_OUTPUT} | grep "status not compatible with the JOB_CANCEL command"`
+if [ -z "$RESULT" ]; then
+  failure "Error message is not as expected: ${COM_OUTPUT}"
   ((FAILED++)) # continue
 else
   run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-job-cancel -N $JOBID
@@ -35,60 +69,72 @@ else
   fi
 fi
 
-my_echo "TEST 1: submit and then cancel jobs with --all option:"
+####
+
+my_echo "TEST 3: check if the --all option works:"
 
 wait_until_job_runs
+
+run_command "${TESTCOMMAND} --noint --all --endpoint $ENDPOINT"
 if [ $? -ne 0 ]; then
-  failure ${COM_OUTPUT}
-  ((FAILED++)) # continue
+  exit_failure "Command failed: ${COM_OUTPUT}"
 else
-  run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-job-cancel -N --all -e $ENDPOINT
-  if [ $? -ne 0 ]; then
-    failure ${COM_OUTPUT}
-    ((FAILED++)) # continue
-  else
-    success
-  fi
+  success
 fi
 
-my_echo "TEST 2: try to cancel a terminated job:"
+####
 
-wait_until_job_finishes
+my_echo "TEST 4: check the requirements of the --all option (3 cases):"
 
-run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-job-cancel -N $JOBID
-RESULT=`echo ${COM_OUTPUT} | grep "status not compatible with the JOB_CANCEL command"`
-if [ -z "$RESULT" ]; then
-  failure ${COM_OUTPUT}
+# -a and -i are not compatible
+run_command "${TESTCOMMAND} --nomsg  --all --input $MYTMPDIR/jobid"
+if [ $? -ne 1 ]; then
+  exit_failure "Command unexpected success: ${COM_OUTPUT}"
+fi
+
+# check the error message
+TMP=`echo ${COM_OUTPUT} | grep "all and --input or --all and specification of JobID(s) as argument are exclusive"`
+if [ $? -ne 0 ]; then
+  failure " The ouput of the command is: ${COM_OUTPUT}"
   ((FAILED++)) # continue
 else
   success
 fi
 
-my_echo "TEST 3: submit and cancel a job and append the output to the existing file ${LOGFILE}:";
-echo "#HEADER#" > ${LOGFILE} || exit_failure "Cannot open ${LOGFILE}";
-
-wait_until_job_runs
-if [ $? -ne 0 ]; then
-  failure ${COM_OUTPUT}
-  ((FAILED++)) # continue
-else
-  run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-job-cancel --logfile ${LOGFILE} -d -N $JOBID
-  RESULT=`grep "#HEADER#" ${LOGFILE}`
-  if [ -z "$RESULT" ]; then
-    failure "File ${LOGFILE} has been overwrite"
-    ((FAILED++)) # continue
-  else
-    RESULT=`grep -o -P "INFO|ERROR|WARN|NOTICE" ${LOGFILE}`
-    if [ -z "$RESULT" ]; then
-      failure "Cannot log on file ${LOGFILE}"
-      ((FAILED++)) # continue
-    else
-      success
-    fi
-  fi
+# -a requires -e
+run_command "${TESTCOMMAND} --nomsg  --all"
+if [ $? -ne 1 ]; then
+  exit_failure "Command unexpected success: ${COM_OUTPUT}"
 fi
 
-my_echo "TEST 4: submit and cancel a job specifying a client configuration file:"
+# check the error message
+TMP=`echo ${COM_OUTPUT} | grep "Option --all requires the specification of the endpoint"`
+
+if [ $? -ne 0 ]; then
+  failure " The ouput of the command is: ${COM_OUTPUT}"
+  ((FAILED++)) # continue
+else
+  success
+fi
+
+# -a and JOBID are not compatible
+run_command "${TESTCOMMAND} --nomsg  --all $JOBID"
+if [ $? -ne 1 ]; then
+  exit_failure "Command unexpected success: ${COM_OUTPUT}"
+fi
+
+# check the error message
+TMP=`echo ${COM_OUTPUT} | grep "all and --input or --all and specification of JobID(s) as argument are exclusive"`
+if [ $? -ne 0 ]; then
+  failure " The ouput of the command is: ${COM_OUTPUT}"
+  ((FAILED++)) # continue
+else
+  success
+fi
+
+####
+
+my_echo "TEST 5: check if the --conf option works:"
 mkdir ${MYTMPDIR}/cancel_log_dir || exit_failure "Cannot create ${MYTMPDIR}/cancel_log_dir";
 printf "[
 CANCEL_LOG_DIR=\"${MYTMPDIR}/cancel_log_dir\";
@@ -96,11 +142,9 @@ CREAM_URL_PREFIX=\"https://\";
 CREAM_URL_POSTFIX=\"ce-cream/services/CREAM2\";
 ]
 " > ${MYTMPDIR}/cancel.conf
-run_command ${GLITE_LOCATION:-/opt/glite}/bin/glite-ce-job-cancel --debug \
---conf ${MYTMPDIR}/cancel.conf -N $JOBID
+run_command "${TESTCOMMAND} --debug --conf ${MYTMPDIR}/cancel.conf --noint $JOBID"
 if [ $? -ne 0 ]; then
-  failure ${COM_OUTPUT}
-  ((FAILED++)) # continue
+  exit_failure "Command failed: ${COM_OUTPUT}"
 else
   RESULT=`ls ${MYTMPDIR}/cancel_log_dir/* | grep glite-ce-job-cancel_CREAM | wc -l 2>/dev/null`
   if [ $RESULT == "0" ]; then
@@ -111,13 +155,32 @@ else
   fi
 fi
 
+####
 
+my_echo "TEST 6: save info into a logfile to check if --debug and --logfile options work:";
+echo "#HEADER#" > ${LOGFILE} || exit_failure "Cannot open ${LOGFILE}";
 
+wait_until_job_runs
 
+run_command "${TESTCOMMAND} --logfile ${LOGFILE} --debug --noint $JOBID"
+RESULT=`grep "#HEADER#" ${LOGFILE}`
+if [ -z "$RESULT" ]; then
+  failure "File ${LOGFILE} has been overwritten"
+  ((FAILED++)) # continue
+else
+  RESULT=`grep -o -P "INFO|ERROR|WARN|NOTICE" ${LOGFILE}`
+  if [ -z "$RESULT" ]; then
+    failure "Cannot log on file ${LOGFILE}"
+    ((FAILED++)) # continue
+  else
+    success
+  fi
+fi
 
+#### FINISHED
 
 if [ $FAILED -gt 0 ] ; then
-  exit_failure "$FAILED test(s) failed on 5 differents tests"
+  exit_failure "$FAILED test(s) failed on 8 differents tests"
 else
   exit_success
 fi
