624e167667b0f1ff14db96947949f911cd1ff02e
Luca Petronzio <luca.petronzio@telespazio.com>
2008-06-13 14:53:30 +0000

    versioning modified: updated through etics properties

--- org.glite.wmproxy/update_version.sh
+++ org.glite.wmproxy/update_version.sh
@@ -1,9 +1,1 @@
-IFS=",";
-grep "AC_INIT" configure.ac > output;
-read foo version<output;
-IFS="[.]) ";
-echo $version>output;
-IFS=" ";
-read maj min rel<output;
-rm output;
-awk -v MAJ=$maj -v MIN=$min -v REL=$rel '{gsub ("MAJOR",MAJ,$0);gsub ("MINOR",MIN,$0);gsub ("RELEASE",REL,$0);print $0}' < src/server/versions.h.in >destination;mv destination src/server/versions.h 
+awk -v MAJ="$1" -v MIN="$2" -v REL="$3" '{gsub ("MAJOR",MAJ,$0);gsub ("MINOR",MIN,$0);gsub ("RELEASE",REL,$0);print $0}' < src/server/versions.h.in >destination;mv destination src/server/versions.h 
