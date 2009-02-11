6a81f378ea37b75b086933710cb4a863c9c38c02
pucciani <pucciani>
2009-02-11 14:07:29 +0000

    Fixes to solve the index=10 issue

--- org.glite.testsuites.ctb/SCAS/stress/proxy_renewal.sh
+++ org.glite.testsuites.ctb/SCAS/stress/proxy_renewal.sh
@@ -15,43 +15,51 @@ fi
 
 #TODO remove hardcoded path
 echo "Retrieving test user cert and key"
-cp --reply=yes $users_certs_dir/test_user_50${INDEX}_cert.pem ./
-cp --reply=yes $users_certs_dir/test_user_50${INDEX}_key.pem ./
-chown root ./test_user_50${INDEX}_cert.pem
-chown root ./test_user_50${INDEX}_key.pem
+
+if [ $INDEX -eq 10 ]; then
+  usernum=510
+else
+  usernum=50${INDEX}
+fi
+
+#cp --reply=yes $users_certs_dir/test_user_50${INDEX}_cert.pem ./
+cp --reply=yes $users_certs_dir/test_user_${usernum}_cert.pem ./
+cp --reply=yes $users_certs_dir/test_user_${usernum}_key.pem ./
+chown root ./test_user_${usernum}_cert.pem
+chown root ./test_user_${usernum}_key.pem
 
 export PATH="$PATH:/opt/glite/bin/"
 
-echo "Creating proxy file x509up_u501_$INDEX"  
-echo "test" | glite-voms-proxy-init -q --voms dteam -cert ./test_user_50${INDEX}_cert.pem -key test_user_50${INDEX}_key.pem -out ./x509up_u501_$INDEX -pwstdin 
+echo "Creating proxy file x509up_u501_${usernum}"  
+echo "test" | glite-voms-proxy-init -q --voms dteam -cert ./test_user_${usernum}_cert.pem -key test_user_${usernum}_key.pem -out ./x509up_u501_${usernum} -pwstdin 
 #true
 if [ $? -ne 0 ]; then
   echo "Error creating the proxy" 
   exit 1
 fi
-cp --reply=yes ./x509up_u501_$INDEX /home/dteamdteampilot$INDEX
+cp --reply=yes ./x509up_u501_${usernum} /home/dteamdteampilot${usernum}
 if [ $? -ne 0 ]; then
   echo "Error copying the proxy" 
   exit 1
 fi
-echo "User proxy ./x509up_u501_$INDEX succesfully created and stored in pilot user account" 
+echo "User proxy ./x509up_u501_${usernum} succesfully created and stored in pilot user account" 
 
 echo "Entering the loop" 
 while [ 1 ] 
 do
   sleep 4h
-  echo "Creating proxy file x509up_u501_$INDEX" 
-  echo "test" | glite-voms-proxy-init -q --voms dteam -cert ./test_user_50${INDEX}_cert.pem -key test_user_50${INDEX}_key.pem -out ./x509up_u501_$INDEX -pwstdin 
+  echo "Creating proxy file x509up_u501_${usernum}" 
+  echo "test" | glite-voms-proxy-init -q --voms dteam -cert ./test_user_${usernum}_cert.pem -key test_user_${usernum}_key.pem -out ./x509up_u501_${usernum} -pwstdin 
   if [ $? -ne 0 ]; then
     echo "Error creating the proxy" 
     break #do not exit, could be a temporary problem
   fi
-  cp --reply=yes ./x509up_u501_$INDEX /home/dteamdteampilot$INDEX
+  cp --reply=yes ./x509up_u501_${usernum} /home/dteamdteampilot${usernum}
   if [ $? -ne 0 ]; then
     echo "Error copying the proxy" 
     exit 1
   fi
-  echo "User proxy ./x509up_u501_$INDEX succesfully created and stored in pilot user account" 
+  echo "User proxy ./x509up_u501_${usernum} succesfully created and stored in pilot user account" 
   echo "Let's get the AFS token too then!"
   echo "Using get_afs_token script: $get_afstoken_script" 
   afs_pass=`cat $afs_pass_file`
