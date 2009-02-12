59e1712ee1b0a22a615bc9980326ff753543726a
Salvatore Monforte <salvatore.monforte@ct.infn.it>
2009-02-12 09:53:16 +0000

    *** empty log message ***

---
+++ org.glite.wms.helper/src/broker/Helper_matcher_ism.h
@@ -0,0 +1,34 @@
+// File: Helper.h
+// Author: Francesco Giacomini <Francesco.Giacomini@cnaf.infn.it>
+// Copyright (c) 2002 EU DataGrid.
+// For license conditions see http://www.eu-datagrid.org/license.html
+
+// $Id$
+
+#ifndef GLITE_WMS_HELPER_MATCHER_HELPER_H
+#define GLITE_WMS_HELPER_MATCHER_HELPER_H
+
+#include "glite/wms/helper/HelperImpl.h"
+#include <boost/shared_ptr.hpp>
+
+namespace glite {
+namespace wms {
+namespace helper {
+namespace matcher {
+class NoCompatibleCEs;
+class Helper: public helper::HelperImpl
+{
+public:
+
+  std::string id() const;
+  std::string output_file_suffix() const;
+  classad::ClassAd* resolve(
+    classad::ClassAd const* input_ad,
+    boost::shared_ptr<std::string> jwt = boost::shared_ptr<std::string>()
+  ) const;
+};
+
+}}}} // glite::wms::helper::matcher
+
+#endif
+
