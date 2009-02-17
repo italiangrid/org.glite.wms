f0f0bcd44ada6f9fc131562cbf3ac863a2864226
pucciani <pucciani>
2009-02-17 09:42:25 +0000

    fixes to AFS token renewal daemon

--- org.glite.testsuites.ctb/SCAS/stress/token_renewal.sh
+++ org.glite.testsuites.ctb/SCAS/stress/token_renewal.sh
@@ -9,6 +9,16 @@ if [ $# -ne 0 ]; then
   exit 1
 fi
 
+source setup_test.cfg
+
+echo "Using get_afs_token script: $get_afstoken_script" 
+afs_pass=`cat $afs_pass_file`
+expect $get_afstokenlocal_script $afs_user $afs_pass
+if [ $? -ne 0 ]; then
+  echo "Error getting AFS token" 
+  break #do not exit, could be a temporary problem
+fi
+echo "AFS token got" 
 
 echo "Entering the loop" 
 while [ 1 ] 
