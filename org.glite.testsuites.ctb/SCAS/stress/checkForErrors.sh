22f4b32c416c6085fe0ab0fb3755c02472901448
pucciani <pucciani>
2009-03-06 13:06:09 +0000

    new file added

---
+++ org.glite.testsuites.ctb/SCAS/stress/checkForErrors.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+#This file produce the correct results only when using 
+#during the test, not at the end of the test
+
+function usage() {
+
+echo "Usage: $0 <logdir>"
+echo "  <logdir> the directory where the log files are stored"
+}
+
+if [ $# -ne 1 ]; then
+  echo "Wrong arguments"
+  usage
+  exit 1
+fi
+
+if [ -d $1 ]; then
+  logdir=$1
+  pushd $logdir >> /dev/null
+else
+  echo "The argument provided is not a valid directory"
+  exit 1
+fi
+
+rm -f tmpfile
+touch tmpfile
+
+for j in `seq 5  6`; do 
+  for i in `seq 1  5`; do 
+    cat lxb760${j}v${i}.cern.ch | sort | uniq | wc -l >> tmpfile; 
+  done; 
+done
+
+#In tmpfile I should have 10 rows with '11'
+errors=`egrep  '[^11]' tmpfile | wc -l`
+
+echo "Errors: $errors"
+
+popd >> /dev/null
+
+exit 0
