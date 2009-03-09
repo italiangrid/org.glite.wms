f43ed967a801f5bde0f06a593e6bb9b6fe8eda9c
pucciani <pucciani>
2009-03-09 14:38:42 +0000

    added tests

--- org.glite.testsuites.ctb/SCAS/check_client_error.sh
+++ org.glite.testsuites.ctb/SCAS/check_client_error.sh
@@ -15,23 +15,9 @@ else
 fi
 
 fail=0
+export GLITE_LOCATION=/opt/glite
 
-##################################################################
-echo "Test 1:"
-echo "Check that glexec returns 201 when no proxy is provided"
-unset GLEXEC_CLIENT_CERT
-unset GLEXEC_SOURCE_PROXY
-unset X509_USER_PROXY
-$GLITE_LOCATION/sbin/glexec "/usr/bin/whoami"
-ret=$?
-if [ $ret -ne 201 ]; then
-  echo "Return code expected was 201 but $ret was given"
-  echo "Test 1 failed"
-  fail=1
-fi
-##################################################################
-echo "Test 2:"
-echo "Check that glexec returns 201 when user proxy is expired"
+function createProxy() {
 
 keyfile="test_user_${usernum}_key.pem"
 certfile="test_user_${usernum}_cert.pem"
@@ -43,30 +29,128 @@ if [ ! -f $keyfile ] || [ ! -f $certfile ]; then
 fi
 
 echo "Creating proxy file x509up_u501_${usernum}"  
-echo "test" | glite-voms-proxy-init -valid 0:1 -q --voms dteam -cert ./test_user_${usernum}_cert.pem -key test_user_${usernum}_key.pem -out ./x509up_u501_${usernum} -pwstdin
+echo "test" | glite-voms-proxy-init -q --voms dteam -cert ./test_user_${usernum}_cert.pem -key test_user_${usernum}_key.pem -out ./x509up_u501_${usernum} -pwstdin
 if [ $? -ne 0 ]; then
   echo "Error creating the proxy" 
-  fail=1
+  exit 1
 else
-  echo "Sleep 2 minutes to let the proxy expires"
-  sleep 2m
-  export GLEXEC_CLIENT_CERT=$proxy
-  export GLEXEC_SOURCE_PROXY=$proxy
-  export  X509_USER_PROXY=$proxy
-  $GLITE_LOCATION/sbin/glexec "/usr/bin/whoami"
-  ret=$?
-  if [ $ret -ne 201 ]; then
-    echo "Return code expected was 201 but $ret was given"
-    echo "Test 1 failed"
-    fail=1
-  else
-    echo "Test 2 passed"
-  fi
+  echo "Proxy created"
+fi
+}
+
+createProxy
+proxy="x509up_u501_${usernum}"
+
+echo "Check that glexec works with correct settings"
+export GLEXEC_CLIENT_CERT=$proxy
+export GLEXEC_SOURCE_PROXY=$proxy
+export X509_USER_PROXY=$proxy
+$GLITE_LOCATION/sbin/glexec "/usr/bin/whoami"
+ret=$?
+if [ $ret -ne 0 ]; then
+  echo "Return code expected was 0 but $ret was given"
+  echo "Pre-testfailed (proxy was $proxy)"
+  exit 1
+else
+  echo "Pre-test OK"
+fi
+
+##################################################################
+echo "Test 1:"
+echo "Check that glexec returns 201 when no proxy is provided"
+rm -f $proxy
+unset GLEXEC_CLIENT_CERT
+unset GLEXEC_SOURCE_PROXY
+unset X509_USER_PROXY
+$GLITE_LOCATION/sbin/glexec "/usr/bin/whoami"
+ret=$?
+if [ $ret -ne 201 ]; then
+  echo "Return code expected was 201 but $ret was given"
+  echo "Test 1 failed (Know Issue)"
+  fail=1
 fi
 ##################################################################
 
+createProxy
+
+##################################################################
+echo "Test 2:"
+echo "Check that glexec returns 201 with wrong proxy permissions"
+
+chmod 666 $proxy
+GLEXEC_CLIENT_CERT=$proxy
+GLEXEC_SOURCE_PROXY=$proxy
+X509_USER_PROXY=$proxy
+
+$GLITE_LOCATION/sbin/glexec "/usr/bin/whoami"
+ret=$?
+if [ $ret -ne 201 ]; then
+  echo "Return code expected was 201 but $ret was given"
+  echo "Test 2 failed (known issue)"
+  fail=1
+else
+  echo "Test 2 passed"
+fi
+
+chmod 600 $proxy
+
+##################################################################
+
+echo "Test 3:"
+echo "Check that glexec returns 201 when target location is not accessible"
+
+export GLEXEC_CLIENT_CERT=$proxy
+export GLEXEC_SOURCE_PROXY=$proxy
+export X509_USER_PROXY=$proxy
+export GLEXEC_TARGET_PROXY="/root"
+
+$GLITE_LOCATION/sbin/glexec "/usr/bin/whoami"
+ret=$?
+if [ $ret -ne 201 ]; then
+  echo "Return code expected was 201 but $ret was given"
+  echo "Test 3 failed"
+  fail=1
+else
+  echo "Test 3 passed"
+fi
 
 
+
+##################################################################
+#echo "Test 10:"
+#echo "Check that glexec returns 201 when user proxy is expired"
+
+#keyfile="test_user_${usernum}_key.pem"
+#certfile="test_user_${usernum}_cert.pem"
+#proxy="x509up_u501_${usernum}"
+#if [ ! -f $keyfile ] || [ ! -f $certfile ]; then
+#  echo "test user certificate r key not found"
+#  echo "Test 2 failed"
+#  fail=1
+#fi
+
+#echo "Creating proxy file x509up_u501_${usernum}"  
+#echo "test" | glite-voms-proxy-init -valid 0:1 -q --voms dteam -cert ./test_user_${usernum}_cert.pem -key test_user_${usernum}_key.pem -out ./x509up_u501_${usernum} -pwstdin
+#if [ $? -ne 0 ]; then
+#  echo "Error creating the proxy" 
+#  fail=1
+#else
+#  echo "Sleep 2 minutes to let the proxy expires"
+#  sleep 2m
+#  export GLEXEC_CLIENT_CERT=$proxy
+#  export GLEXEC_SOURCE_PROXY=$proxy
+#  export  X509_USER_PROXY=$proxy
+#  $GLITE_LOCATION/sbin/glexec "/usr/bin/whoami"
+#  ret=$?
+#  if [ $ret -ne 201 ]; then
+#    echo "Return code expected was 201 but $ret was given"
+#    echo "Test 2 failed"
+#    fail=1
+#  else
+#    echo "Test 2 passed"
+#  fi
+#fi
+##################################################################
 
 
 if [ $fail -eq 0 ];then
@@ -77,3 +161,4 @@ else
   exit 1
 fi
 
+exit 0
