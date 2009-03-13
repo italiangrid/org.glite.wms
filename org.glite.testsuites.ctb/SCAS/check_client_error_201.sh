fdd8469082393bcea127997010a91644097152f4
pucciani <pucciani>
2009-03-13 14:35:07 +0000

    added tests for 203 and 204 exit codes

--- org.glite.testsuites.ctb/SCAS/check_client_error_201.sh
+++ org.glite.testsuites.ctb/SCAS/check_client_error_201.sh
@@ -160,7 +160,7 @@ fi
 echo "Test 6:"
 echo "Check that glexec returns 201 when GLEXEC_CLIENT_CERT is not set"
 
-unset GLEXEC_CLIENT_CERT=$proxy
+unset GLEXEC_CLIENT_CERT
 export GLEXEC_SOURCE_PROXY=$proxy
 export X509_USER_PROXY=$proxy
 unset GLEXEC_TARGET_PROXY
@@ -169,78 +169,13 @@ $GLITE_LOCATION/sbin/glexec "/usr/bin/whoami"
 ret=$?
 if [ $ret -ne 201 ]; then
   echo "Return code expected was 201 but $ret was given"
-  echo "Test 5 failed"
-  fail=`echo "$fail +1" | bc`
-else
-  echo "Test 5 passed"
-fi
-
-
-##################################################################
-
-echo "Test 6:"
-echo "Check that glexec returns 204 when the binary returns the exit codes: 201, 202, 203, 204"
-
-export GLEXEC_CLIENT_CERT=$proxy
-export GLEXEC_SOURCE_PROXY=$proxy
-export X509_USER_PROXY=$proxy
-unset GLEXEC_TARGET_PROXY
-
-#create exit201 script
-cat <<EOF > /tmp/exit201.sh
-#!/bin/bash
-echo "Exit 201 dummy program"
-exit 201
-EOF
-chmod a+x /tmp/exit201.sh
-
-$GLITE_LOCATION/sbin/glexec "/tmp/exit201.sh"
-ret=$?
-if [ $ret -ne 204 ]; then
-  echo "Return code expected was 204 but $ret was given"
   echo "Test 6 failed"
   fail=`echo "$fail +1" | bc`
 else
   echo "Test 6 passed"
 fi
 
-rm -f /tmp/exit201.sh
-
-##################################################################
-#echo "Test 10:"
-#echo "Check that glexec returns 201 when user proxy is expired"
-
-#keyfile="test_user_${usernum}_key.pem"
-#certfile="test_user_${usernum}_cert.pem"
-#proxy="x509up_u501_${usernum}"
-#if [ ! -f $keyfile ] || [ ! -f $certfile ]; then
-#  echo "test user certificate r key not found"
-#  echo "Test 2 failed"
-#  fail=1
-#fi
-
-#echo "Creating proxy file x509up_u501_${usernum}"  
-#echo "test" | glite-voms-proxy-init -valid 0:1 -q --voms dteam -cert ./test_user_${usernum}_cert.pem -key test_user_${usernum}_key.pem -out ./x509up_u501_${usernum} -pwstdin
-#if [ $? -ne 0 ]; then
-#  echo "Error creating the proxy" 
-#  fail=1
-#else
-#  echo "Sleep 2 minutes to let the proxy expires"
-#  sleep 2m
-#  export GLEXEC_CLIENT_CERT=$proxy
-#  export GLEXEC_SOURCE_PROXY=$proxy
-#  export  X509_USER_PROXY=$proxy
-#  $GLITE_LOCATION/sbin/glexec "/usr/bin/whoami"
-#  ret=$?
-#  if [ $ret -ne 201 ]; then
-#    echo "Return code expected was 201 but $ret was given"
-#    echo "Test 2 failed"
-#    fail=1
-#  else
-#    echo "Test 2 passed"
-#  fi
-#fi
-##################################################################
+#################################################################
 
 
 if [ $fail -eq 0 ];then
