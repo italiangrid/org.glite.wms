baa3a4d1d583cd02070f6f0f0e302857fff210e4
Alvise Dorigo <alvise.dorigo@pd.infn.it>
2009-04-20 09:52:58 +0000

    finished tool to query ICE's database

--- org.glite.wms.ice/src/queryDb.cpp
+++ org.glite.wms.ice/src/queryDb.cpp
@@ -11,31 +11,7 @@
 
 #include "boost/algorithm/string.hpp"
 
-using namespace std;
-namespace iceUtil = glite::wms::ice::util;
-namespace db = glite::wms::ice::db;
-void printhelp( void ) {}
-
-// bool validate( const string& field ) {
-//   if( field == "gridjobid" ||
-//       field == "creamjobid" ||
-//       field == "complete_cream_jobid" ||
-//       field == "userdn" ||
-//       field == "delegationid" ||
-//       field == "userproxy" ||
-//       field == "creamurl" ||
-//       field == "myproxyurl" ||
-//       field == "proxy_renewable" ||
-//       field == "failure_reason" ||
-//       field == "status" ||
-//       field == "leaseid" ||
-//       field == "proxycert_timestamp"
-//       )
-//     return true;
-//   return false;
-// }
-
-string status_to_numstr( const std::string& status ) {
+std::string status_to_numstr( const std::string& status ) {
   if(status == "REGISTERED" ) return "0";
   if(status == "PENDING" ) return "1";
   if(status == "IDLE" ) return "2";
@@ -48,10 +24,49 @@ string status_to_numstr( const std::string& status ) {
   if(status == "DONE_FAILED" ) return "9";
   if(status == "UNKNOWN" ) return "10";
   if(status == "PURGED" ) return "11";
-
-  cerr << "Invalid status [" << status << "]. STOP!" << endl;
+  
+  std::cerr << "Invalid status [" << status << "]. STOP!" << std::endl;
   exit(1);
 }
+
+using namespace std;
+namespace iceUtil = glite::wms::ice::util;
+namespace db = glite::wms::ice::db;
+void printhelp( void ) {
+  cout << "USAGE: queryDb --conf|-c <WMS CONFIGURATION FILE> [options]" << endl;
+  cout << endl << "options: " << endl;
+  cout << "  --verbose|-v\t\tVerbose output (print each db's record" << endl;
+  //  cout << "\t--debug|-d\tPrint the SQL query before executing it" << endl;
+  cout << "  --status-filter|-s\tSelect only records in which the status column is one"<<endl
+       << "  \t\t\tof those specified as option argument; more states can"<<endl
+       << "  \t\t\tbe ',' separated and they must be:"<<endl;
+  cout << "\t\t\tREGISTERED"<<endl;
+  cout << "\t\t\tREGISTERED"<<endl;
+  cout << "\t\t\tPENDING"<<endl;
+  cout << "\t\t\tIDLE"<<endl;
+  cout << "\t\t\tRUNNING"<<endl;
+  cout << "\t\t\tREALLY-RUNNING"<<endl;
+  cout << "\t\t\tCANCELLED"<<endl;
+  cout << "\t\t\tHELD"<<endl;
+  cout << "\t\t\tABORTED"<<endl;
+  cout << "\t\t\tDONE_OK"<<endl;
+  cout << "\t\t\tDONE_FAILED"<<endl;
+  cout << "\t\t\tUNKNOWN"<<endl;
+  cout << "\t\t\tPURGED"<<endl<<endl;
+  cout << "  --userdn|-u\t\tPrint the USERDN column of the job table" << endl;
+  cout << "  --creamjobid|-C\tPrint the CREAM JOB ID column of the job table" << endl;
+  cout << "  --gridjobid|-G\tPrint the GRID JOB ID column of the job table" << endl;
+  cout << "  --userproxy|-p\tPrint the USER PROXY column of the job table" << endl;
+  cout << "  --cream-url|-r\tPrint the CREAM URL column of the job table" << endl;
+  cout << "  --myproxy-url|-m\tPrint the MYPROXY URL column of the job table" << endl;
+  cout << "  --status|-S\t\tPrint the STATUS column of the job table" << endl;
+  cout << "  --lease-id|-L\t\tPrint the LEASE-ID column of the job table" << endl;
+  cout << "  --delegation-id|-D\tPrint the DELEGATION-ID column of the job table" << endl;
+  cout << "  --proxy-exptime|-t\tPrint the PROXY EXPIRATION TIME column of the job table" << endl;
+  cout << "  --worker-node|-w\tPrint the WORKER-NODE column of the job table" << endl;
+  cout << "  --help|-h\t\tPrint this help" << endl;
+}
+
 
 int main( int argc, char* argv[] )
 {
@@ -60,6 +75,7 @@ int main( int argc, char* argv[] )
   string           sqlquery("");
   bool             all_status(true);
   bool             debug( false );
+  bool             verbose( false );
   int              option_index( 0 );
   list< string >   fields_to_retrieve;
 
@@ -67,7 +83,6 @@ int main( int argc, char* argv[] )
     int c;
     static struct option long_options[] = {
       {"help", 0, 0, 'h'},
-      {"debug", 0, 0, 'd'},
       {"status-filter", 1, 0, 's'},
       {"conf", 1, 0, 'c'},
       /*****************************/
@@ -82,9 +97,10 @@ int main( int argc, char* argv[] )
       {"delegation-id",0,0,'D'},
       {"proxy-exptime",0,0,'t'},
       {"worker-node",0,0,'w'},
+      {"verbose", 0, 0, 'v'},
       {0, 0, 0, 0}
     };
-    c = getopt_long(argc, argv, "hds:c:uCGprmSLDtw", long_options, &option_index);
+    c = getopt_long(argc, argv, "hs:c:uCGprmSLDtwv", long_options, &option_index);
     
     if ( c == -1 )
       break;
@@ -107,7 +123,7 @@ int main( int argc, char* argv[] )
       fields_to_retrieve.push_back( "userdn" );
       break;
     case 'C':
-      fields_to_retrieve.push_back( "creamjobid" );
+      fields_to_retrieve.push_back( "complete_cream_jobid" );
       break;
     case 'G':
       fields_to_retrieve.push_back( "gridjobid" );
@@ -136,6 +152,9 @@ int main( int argc, char* argv[] )
     case 'w':
       fields_to_retrieve.push_back( "worker_node" );
       break;
+    case 'v':
+      verbose = true;
+      break;
     default:
       cerr << "Type " << argv[0] << " --help|-h for an help" << endl;
       exit(1);
@@ -153,23 +172,6 @@ int main( int argc, char* argv[] )
   }
 
   vector< string > _fields_array;
-//   boost::split(_fields_array, fields_to_retrieve, boost::is_any_of(","));
-
-//   if( !_fields_array.size() ) exit( 0 );
-
-//   for(vector< string >::const_iterator it = _fields_array.begin();
-//       it != _fields_array.end();
-//       ++it )
-//     {
-//       if( !validate( *it ) ) {
-// 	cerr << "Field [" << *it << "] is not defined in the job table of ICE. STOP!" << endl;
-// 	exit(1);
-//       }
-//     }
-
-//   list< string > fields_list;
-
-//   copy( fields_to_retrieve.begin(), fields_to_retrieve.end(), back_inserter( fields_array ) );
 
   vector< string > _states_array;
   boost::split(_states_array, states, boost::is_any_of(","));
@@ -195,15 +197,21 @@ int main( int argc, char* argv[] )
   db::Transaction tnx;
   tnx.execute( &getter );
   list< vector< string > > result = getter.get_values();
+  if(verbose) {
+    for( list< vector< string > >::const_iterator it=result.begin();
+	 it != result.end();
+	 ++it)
+      {
+	for( vector< string >::const_iterator jt=it->begin();
+	     jt != it->end();
+	     ++jt)
+	  {
+	    cout << "[" << *jt << "] ";
+	  }
+	cout << endl;
+      }
+    cout << endl << "------------------------------------------------" << endl;
+  }
   cout << result.size() << " item(s) found" << endl;
 
-  // 
-//   db::GetFields getter( fields_array, params );
-//   db::Transaction tnx;
-//   tnx.execute( &getter );
-// 
-//   list< vector< string > > result = getter.get_values();
-// 
-//   cout << result.size() << " item(s) found" << endl;
-  
 }
