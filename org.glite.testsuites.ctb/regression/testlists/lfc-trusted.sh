de96567951bbcc69b7c752699d25559d705ec88d
dshiyach <dshiyach>
2009-05-04 08:14:48 +0000

    LFC regression tests

---
+++ org.glite.testsuites.ctb/regression/testlists/lfc-trusted.sh
@@ -0,0 +1,6 @@
+DPM_TESTHOST=ctb-generic-7.cern.ch
+DPNS_TESTHOST=ctb-generic-7.cern.ch
+LFC_TESTHOST=ctb-generic-16.cern.ch
+LFC_TESTHOME=/grid/org.glite.voms-test
+LOCAL_FILE=/bin/ls
+TEST_VO=org.glite.voms-test
