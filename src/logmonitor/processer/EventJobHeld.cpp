b6d908de403532e9d07e763920d37427a2d52677
Fabio Capannini <fabio.capannini@cnaf.infn.it>
2009-06-16 13:30:55 +0000

    unnecessary include removed

--- src/logmonitor/processer/EventJobHeld.cpp
+++ src/logmonitor/processer/EventJobHeld.cpp
@@ -1,3 +1,16 @@
+// Copyright (c) Members of the EGEE Collaboration. 2009. 
+// See http://www.eu-egee.org/partners/ for details on the copyright holders.  
+
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -12,7 +25,6 @@
 #include <globus_gram_protocol_constants.h>
 #endif
 
-#include "glite/wmsutils/jobid/JobId.h"
 #include "glite/wms/common/logger/logstream.h"
 #include "glite/wms/common/logger/manipulators.h"
 #include "common/EventLogger.h"
