030accb45ec8019b39f05da7836a266c51354bea
Alessio Gianelle <alessio.gianelle@pd.infn.it>
2009-10-08 14:54:11 +0000

    Add cream requirements

--- org.glite.testsuites.ctb/WMS/regression/31012.sh
+++ org.glite.testsuites.ctb/WMS/regression/31012.sh
@@ -3,4 +3,4 @@
 # Check the version returned by the server and the ones of the command.
 # They must be the same number
 
-glite-wms-job-submit --debug --version --config etc/wmp_devel19.conf  | grep " Version" | sed -e "s/.*: //"
+glite-wms-job-submit --debug --version --config ~/UI/etc/wmp_devel19.conf  | grep " Version" | sed -e "s/.*: //"
