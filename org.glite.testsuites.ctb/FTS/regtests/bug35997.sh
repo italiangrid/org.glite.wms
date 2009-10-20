6575c761fa582b8f3cc9fe7e1d3abbf7f772ae2e
pucciani <pucciani>
2009-10-20 09:52:56 +0000

    test updated

--- org.glite.testsuites.ctb/FTS/regtests/bug35997.sh
+++ org.glite.testsuites.ctb/FTS/regtests/bug35997.sh
@@ -10,45 +10,72 @@ pushd /opt/glite/bin
 
 for i in glite-transfer*; 
 do 
-  echo "\t$i -s httpj://foo"
-  message=`./$i -s httpj://foo 2>&1`
+  echo "$i -s httpj://foo"
+  if [ "$i" == "glite-transfer-service-info" ]; then
+    message=`./$i --version -s httpj://foo 2>&1`
+  else
+    message=`./$i -s httpj://foo 2>&1`
+  fi
   echo $message | grep 'Failed to determine the endpoint: Wrongly formatted endpoint'
   if [ $? -ne 0 ]; then
     echo "BUG PRESENT"
     exit 1
+  else
+    echo "OK"
   fi
 done
 
 for i in glite-transfer*;
 do 
-  echo "\t$i  -s https://foo"; 
-  message=`./$i -s https://foo 2>&1`
+  echo "$i  -s https://foo"; 
+  if [ "$i" == "glite-transfer-service-info" ]; then
+    message=`./$i --version -s https://foo 2>&1`
+  else
+    message=`./$i -s https://foo 2>&1`
+  fi
   echo $message | grep 'could not open connection to'
   if [ $? -ne 0 ]; then
     echo "BUG PRESENT"
     exit 1
+  else
+    echo "OK"
   fi
 done
 
 for i in glite-transfer*; 
 do 
-  echo "\t$i  -s httpg://foo"; 
-  message=`./$i -s httpg://foo 2>&1`   
+  echo "$i  -s httpg://foo"; 
+  if [ "$i" == "glite-transfer-service-info" ]; then
+    message=`./$i --version -s httpg://foo 2>&1`
+  else
+    message=`./$i -s httpg://foo 2>&1`
+  fi
   echo $message | grep 'could not open connection to'
   if [ $? -ne 0 ]; then
     echo "BUG PRESENT"
     exit 1
+  else
+    echo "OK"
   fi
 done
 
 for i in glite-transfer*;
 do
-  echo "\t$i -s http://foo"; 
-  message=`./$i -s http://foo 2>&1`   
+  echo "$i -s http://foo"; 
+  if [ "$i" == "glite-transfer-service-info" ]; then
+    message=`./$i --version -s http://foo 2>&1`
+  else
+    message=`./$i -s http://foo 2>&1`
+  fi
   echo $message | grep 'could not open connection to'
   if [ $? -ne 0 ]; then
-    echo "BUG PRESENT"
-    exit 1
+    echo $message | grep 'Name or service not known'
+    if [ $? -ne 0 ]; then
+      echo "BUG PRESENT"
+      exit 1
+    fi
+  else
+    echo "OK"
   fi
 done 
 
