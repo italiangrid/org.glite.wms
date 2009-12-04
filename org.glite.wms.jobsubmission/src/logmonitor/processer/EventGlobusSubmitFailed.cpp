66f132f8d11dbd266adb96782f54c96cd517bbab
Marco Cecchi <marco.cecchi@cnaf.infn.it>
2009-12-04 10:58:07 +0000

    rolled back globus error 10 workaround

--- org.glite.wms.jobsubmission/src/logmonitor/processer/EventGlobusSubmitFailed.cpp
+++ org.glite.wms.jobsubmission/src/logmonitor/processer/EventGlobusSubmitFailed.cpp
@@ -1,11 +1,24 @@
+// Copyright (c) Members of the EGEE Collaboration. 2009. 
+// See http://www.eu-egee.org/partners/ for details on the copyright holders.  
+
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+
 #include <cstdio>
 #include <ctime>
+
 #include <memory>
 #include <string>
 
 #include <user_log.c++.h>
 
-#include "glite/wmsutils/jobid/JobId.h"
 #include "glite/wms/common/logger/logstream.h"
 #include "glite/wms/common/logger/manipulators.h"
 #include "common/IdContainer.h"
@@ -22,24 +35,29 @@ using namespace std;
 USING_COMMON_NAMESPACE;
 RenameLogStreamNS( elog );
 
+//namespace {
+//  std::string globus_error10 = "10 data transfer to the server failed";
+//reason: 37 the provided RSL 'queue' parameter
+//reason: 10 data transfer to the server failed
+//}
+
 JOBCONTROL_NAMESPACE_BEGIN {
 
 namespace logmonitor { namespace processer {
 
-EventGlobusSubmitFailed::EventGlobusSubmitFailed(
-  ULogEvent *event, MonitorData *data ) : EventInterface( event, data ), 
-  egsf_event( dynamic_cast<GlobusSubmitFailedEvent *>(event))
-{ }
+EventGlobusSubmitFailed::EventGlobusSubmitFailed( ULogEvent *event, MonitorData *data ) : EventInterface( event, data ), 
+  egsf_event( dynamic_cast<GlobusSubmitFailedEvent *>(event) )
+{}
 
-EventGlobusSubmitFailed::~EventGlobusSubmitFailed()
-{ }
+EventGlobusSubmitFailed::~EventGlobusSubmitFailed( void )
+{}
 
 void EventGlobusSubmitFailed::process_event( void )
 {
   auto_ptr<SubmitReader>                 reader;
   jccommon::IdContainer::iterator        position;
-  controller::JobController              controller(this->ei_data->md_logger);
-  logger::StatePusher                    pusher(elog::cedglog, "EventGlobusSubmitFailed::process_event()");
+  controller::JobController              controller( *this->ei_data->md_logger );
+  logger::StatePusher                    pusher( elog::cedglog, "EventGlobusSubmitFailed::process_event()" );
 
   elog::cedglog << logger::setlevel( logger::info ) << "Got globus submit failed event." << endl
 		<< "For cluster: " << this->ei_condor << ", reason: " << this->egsf_event->reason << endl;
@@ -62,18 +80,20 @@ void EventGlobusSubmitFailed::process_event( void )
 
     reader.reset( this->createReader(position->edg_id()) );
 
-#ifdef GLITE_WMS_HAVE_LBPROXY
-    this->ei_data->md_logger->set_LBProxy_context( position->edg_id(), position->sequence_code(), position->proxy_file() );
-#else
-    this->ei_data->md_logger->reset_user_proxy( position->proxy_file() ).reset_context( position->edg_id(), position->sequence_code() );
-#endif
+    if (this->ei_data->md_logger->have_lbproxy()) {
+      this->ei_data->md_logger->set_LBProxy_context( position->edg_id(), position->sequence_code(), position->proxy_file() );
+    } else {
+      this->ei_data->md_logger->reset_user_proxy( position->proxy_file() ).reset_context( position->edg_id(), position->sequence_code() );
+    }
     this->ei_data->md_logger->globus_submit_failed_event( reader->to_string(), this->egsf_event->reason, this->ei_data->md_logfile_name );
 
-    elog::cedglog << logger::setlevel( logger::info ) << "Forwarding remove request to JC." << endl;
-
-    controller.cancel( this->egsf_event->cluster, this->ei_data->md_logfile_name.c_str() );
-
-    this->ei_data->md_container->update_pointer( position, this->ei_data->md_logger->sequence_code(), this->egsf_event->eventNumber );
+    //if (std::string(this->egsf_event->reason).substr(0, globus_error10.size()) == globus_error10) {
+      //elog::cedglog << logger::setlevel( logger::info ) << "Here it comes! The infamous globus error 10: __NOT__ forwarding remove request to JC." << endl;
+    //} else {
+      elog::cedglog << logger::setlevel( logger::info ) << "Forwarding remove request to JC." << endl;
+      controller.cancel( this->egsf_event->cluster, this->ei_data->md_logfile_name.c_str() );
+      this->ei_data->md_container->update_pointer( position, this->ei_data->md_logger->sequence_code(), this->egsf_event->eventNumber );
+    //}
   }
 
   return;
@@ -81,4 +101,4 @@ void EventGlobusSubmitFailed::process_event( void )
 
 }} // namespace processer, logmonitor
 
-} JOBCONTROL_NAMESPACE_END;
+} JOBCONTROL_NAMESPACE_END
--- org.glite.wms.jobsubmission/src/logmonitor/processer/EventJobHeld.cpp
+++ org.glite.wms.jobsubmission/src/logmonitor/processer/EventJobHeld.cpp
@@ -1,3 +1,16 @@
+// Copyright (c) Members of the EGEE Collaboration. 2009. 
+// See http://www.eu-egee.org/partners/ for details on the copyright holders.  
+
+// Licensed under the Apache License, Version 2.0 (the "License"); 
+// you may not use this file except in compliance with the License. 
+// You may obtain a copy of the License at 
+//     http://www.apache.org/licenses/LICENSE-2.0 
+// Unless required by applicable law or agreed to in writing, software 
+// distributed under the License is distributed on an "AS IS" BASIS, 
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+// See the License for the specific language governing permissions and 
+// limitations under the License.
+
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -12,9 +25,16 @@
 #include <globus_gram_protocol_constants.h>
 #endif
 
-#include "glite/wmsutils/jobid/JobId.h"
+#include "glite/jobid/JobId.h"
+
+#include "glite/wms/common/configuration/Configuration.h"
+#include "glite/wms/common/configuration/NSConfiguration.h"
+
 #include "glite/wms/common/logger/logstream.h"
 #include "glite/wms/common/logger/manipulators.h"
+
+#include "glite/wms/common/utilities/manipulation.h"
+
 #include "common/EventLogger.h"
 #include "common/IdContainer.h"
 #include "controller/JobController.h"
@@ -24,17 +44,22 @@
 #include "EventJobHeld.h"
 #include "MonitorData.h"
 
+//namespace {
+//  std::string const globus_error10 = "Globus error 10";
+//}
+
 using namespace std;
 USING_COMMON_NAMESPACE;
-RenameLogStreamNS(elog);
+
+RenameLogStreamNS( elog );
 
 JOBCONTROL_NAMESPACE_BEGIN {
-namespace logmonitor {
-namespace processer {
+
+namespace logmonitor { namespace processer {
 
 EventJobHeld::EventJobHeld( ULogEvent *event, MonitorData *data ) : EventInterface( event, data ),
 								    ejh_event( dynamic_cast<JobHeldEvent *>(event) )
-{ }
+{}
 
 EventJobHeld::~EventJobHeld( void )
 {}
@@ -43,7 +68,7 @@ void EventJobHeld::process_event( void )
 {
   string                                 reason(this->ejh_event->getReason());
   jccommon::IdContainer::iterator        position;
-  controller::JobController              controller(this->ei_data->md_logger);
+  controller::JobController              controller( *this->ei_data->md_logger );
   logger::StatePusher                    pusher( elog::cedglog, "EventJobHeld::process_event()" );
 
   elog::cedglog << logger::setlevel( logger::info ) << "Got a job held event." << endl
@@ -72,19 +97,73 @@ void EventJobHeld::process_event( void )
 #endif
 		  << ei_s_edgideq << position->edg_id() << endl;
 
-    if( this->ei_data->md_isDagLog )
+    if (this->ei_data->md_isDagLog) {
       elog::cedglog << ei_s_subnodeof << this->ei_data->md_dagId << endl;
+    }
 
-    if( this->ei_data->md_aborted->search(this->ei_condor) ) // Job got a previous error, ignoring event...
+    //if (reason.substr(0, globus_error10.size()) == globus_error10) {
+
+    //  elog::cedglog << logger::setlevel( logger::info ) << "This error is likely to be transient." << endl
+    //    << "Let's retry releasing cluster " << this->ei_condor << " before giving up" << endl;
+
+    //  int const MAX_CONDOR_RETRIES = 30;
+
+    //  std::string const jobid(position->edg_id());
+    //  configuration::Configuration const& config(*configuration::Configuration::instance());
+    //  std::string condor_retries_file(config.ns()->sandbox_staging_path());
+    //  condor_retries_file += "/" + glite::wms::common::utilities::get_reduced_part(jobid);
+    //  condor_retries_file += "/" + glite::wms::common::utilities::to_filename(jobid);
+    //  condor_retries_file += "/Condor.retries";
+
+    //  int condor_retries = 0;
+    //  ifstream ifs(condor_retries_file.c_str());
+    //  if (ifs) {
+    //    ifs >> condor_retries;
+    //    ifs.close();
+    //  }
+    //  ++condor_retries;
+
+    //  ofstream ofs(condor_retries_file.c_str());
+    //  if (ofs) {
+    //    ofs << condor_retries << '\n';
+    //    ofs.close();
+    //  }
+
+    //  if (condor_retries <= MAX_CONDOR_RETRIES) {
+
+    //    elog::cedglog << logger::setlevel( logger::info ) << "Forwarding release request to JC." << endl;
+    //    sleep(5);
+    //    controller.release(this->ejh_event->cluster, this->ei_data->md_logfile_name.c_str());
+
+//#if CONDORG_AT_LEAST(6,5,3)
+    //    this->ei_data->md_container->update_pointer( position, this->ei_data->md_logger->sequence_code(),
+    //    this->ejh_event->eventNumber, this->ejh_event->getReasonSubCode() );
+//#else
+    //    this->ei_data->md_container->update_pointer( position, this->ei_data->md_logger->sequence_code(), this->ejh_event->eventNumber );
+//#endif
+    //   return;
+    //  } else {
+        elog::cedglog << logger::setlevel( logger::info ) << "Number of Condor retries exceeded, __NOT__ forwarding release request to JC." << endl;
+    // }
+    //}
+
+    if (this->ei_data->md_logger->have_lbproxy()) {
+      this->ei_data->md_logger->set_LBProxy_context( position->edg_id(), position->sequence_code(), position->proxy_file() );
+    } else {
+      this->ei_data->md_logger->reset_user_proxy( position->proxy_file() ).reset_context( position->edg_id(), position->sequence_code() );
+    }
+    this->ei_data->md_logger->job_held_event(reason);
+
+    if( this->ei_data->md_aborted->search(this->ei_condor)) {
       elog::cedglog << logger::setlevel( logger::info ) << "The job got a previous error, ignoring the held event..." << endl;
+    }
+
 #if CONDORG_AT_LEAST(6,5,3)
     else if( position->last_status() == GLOBUS_GRAM_PROTOCOL_ERROR_JOB_CANCEL_FAILED ) {
       elog::cedglog << logger::setlevel( logger::warning )
-		    << "Got another held event for an already removing job: Hugh !!!" << endl
-		    << logger::setlevel( logger::debug )
-		    << "I really hate this piece of shit called Condor !!! Sigh !!" << endl
+		    << "Got another held event for an already removing job\n"
 		    << logger::setlevel( logger::info )
-		    << "Ignoring this event..." << endl;
+		    << "Ignoring this event...\n";
     }
     else if( this->ejh_event->getReasonSubCode() == GLOBUS_GRAM_PROTOCOL_ERROR_JOB_CANCEL_FAILED ) {
       elog::cedglog << logger::setlevel( logger::warning )
@@ -99,17 +178,16 @@ void EventJobHeld::process_event( void )
     }
 #endif
     else {
-      if( this->ei_data->md_aborted->insert(this->ei_condor) ) {
-	elog::cedglog << logger::setlevel( logger::fatal ) << ei_s_failedinsertion << endl;
-
-	throw CannotExecute( ei_s_failedinsertion );
+      if (this->ei_data->md_aborted->insert(this->ei_condor)) {
+        elog::cedglog << logger::setlevel( logger::fatal ) << ei_s_failedinsertion << endl;
+	      throw CannotExecute(ei_s_failedinsertion);
       }
 
-#ifdef GLITE_WMS_HAVE_LBPROXY
-      this->ei_data->md_logger->set_LBProxy_context( position->edg_id(), position->sequence_code(), position->proxy_file() );
-#else
-      this->ei_data->md_logger->reset_user_proxy( position->proxy_file() ).reset_context( position->edg_id(), position->sequence_code() );
-#endif
+      if (this->ei_data->md_logger->have_lbproxy()) {
+        this->ei_data->md_logger->set_LBProxy_context( position->edg_id(), position->sequence_code(), position->proxy_file() );
+      } else {
+        this->ei_data->md_logger->reset_user_proxy( position->proxy_file() ).reset_context( position->edg_id(), position->sequence_code() );
+      }
       this->ei_data->md_logger->job_held_event( reason );
 
       elog::cedglog << logger::setlevel( logger::info ) << "Forwarding remove request to JC." << endl;
@@ -131,4 +209,4 @@ void EventJobHeld::process_event( void )
 
 }} // namespace processer, logmonitor
 
-} JOBCONTROL_NAMESPACE_END;
+} JOBCONTROL_NAMESPACE_END
