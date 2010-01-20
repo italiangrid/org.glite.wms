6c1307b96d88da604c66f783093d9027ed2815db
nloullou <nloullou>
2010-01-20 13:42:20 +0000

    Adding License header to Bash Scripts

--- org.glite.testsuites.ctb/SD/tests/CLIManPageTest.sh
+++ org.glite.testsuites.ctb/SD/tests/CLIManPageTest.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
+#
+#  Copyright (c) Members of the EGEE Collaboration. 2009.
+#  See http://public.eu-egee.org/partners/ for details on the
+#  copyright holders.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+ 
 #-------------------------------------------------------
 # Service Discovery CLI MAN page test. It tests 
 # whether a man page exists for glite-sd-query.
-#
-# Script provided by: Nicholas Loulloudes (UCY)
-# Contact: loulloudes.n |_AT_| cs.ucy.ac.cy
 #-------------------------------------------------------
 
 name="glite-sd-query"
