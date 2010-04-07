49258af19a9cc74cf910c8f4a9638dc8aa863d05
Marco Cecchi <marco.cecchi@cnaf.infn.it>
2010-04-07 10:27:33 +0000

    copyright and license applied

--- org.glite.wms.wmproxy-api-java/src/org/glite/wms/wmproxy/WMProxyAPI.java
+++ org.glite.wms.wmproxy-api-java/src/org/glite/wms/wmproxy/WMProxyAPI.java
@@ -19,17 +19,6 @@ limitations under the License.
 
 
 /*
- * Copyright (c) 2005 on behalf of the EU EGEE Project:
- * The European Organization for Nuclear Research (CERN),
- * Istituto Nazionale di Fisica Nucleare (INFN), Italy
- * Datamat Spa, Italy
- * Centre National de la Recherche Scientifique (CNRS), France
- * CS Systeme d'Information (CSSI), France
- * Royal Institute of Technology, Center for Parallel Computers (KTH-PDC), Sweden
- * Universiteit van Amsterdam (UvA), Netherlands
- * University of Helsinki (UH.HIP), Finland
- * University of Bergen (UiB), Norway
- * Council for the Central Laboratory of the Research Councils (CCLRC), United Kingdom
  * 
  * Authors: Marco Sottilaro (marco.sottilaro@datamat.it)
  *
