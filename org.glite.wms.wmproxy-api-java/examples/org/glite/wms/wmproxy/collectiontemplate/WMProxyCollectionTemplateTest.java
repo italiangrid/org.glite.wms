3b56581e8bd3c96d57dcd329d5fbe1fc325ee0a2
Luca Petronzio <luca.petronzio@telespazio.com>
2010-04-09 15:20:13 +0000

    task #13101, [WMS] Review copyright and license...

--- org.glite.wms.wmproxy-api-java/examples/org/glite/wms/wmproxy/collectiontemplate/WMProxyCollectionTemplateTest.java
+++ org.glite.wms.wmproxy-api-java/examples/org/glite/wms/wmproxy/collectiontemplate/WMProxyCollectionTemplateTest.java
@@ -1,13 +1,23 @@
 /*
- * Copyright (c) Members of the EGEE Collaboration. 2004.
- * See http://eu-egee.org/partners/ for details on the copyright holders.
- * For license conditions see the license file or http://eu-egee.org/license.html
-  * author : Marco Sottilaro (marco.sottilaro@datamat.it)
- * Please report any bug at:  egee@datamat.it
- *
- */
+*	Copyright (c) Members of the EGEE Collaboration. 2004.
+*	See http://www.eu-egee.org/partners/ for details on the
+*	copyright holders.
+*
+*	Licensed under the Apache License, Version 2.0 (the "License");
+*	you may not use this file except in compliance with the License.
+*	You may obtain a copy of the License at
+*	 
+*	     http://www.apache.org/licenses/LICENSE-2.0
+*	 
+*	Unless required by applicable law or agreed to in writing, software
+*	distributed under the License is distributed on an "AS IS" BASIS,
+*	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+*	either express or implied.
+*	See the License for the specific language governing permissions and
+*	limitations under the License.
+*/
 
- package org.glite.wms.wmproxy.collectiontemplate ;
+package org.glite.wms.wmproxy.collectiontemplate ;
 
 import org.glite.wms.wmproxy.WMProxyAPI ;
 import org.glite.wms.wmproxy.common.GetParameters ;
