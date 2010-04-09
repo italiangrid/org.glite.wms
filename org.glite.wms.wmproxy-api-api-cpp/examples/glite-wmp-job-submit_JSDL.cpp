8541e250b7d788f23f85324e92ae6f6318b952f3
Luca Petronzio <luca.petronzio@telespazio.com>
2010-04-09 16:26:10 +0000

    task #13101, [WMS] Review copyright and license...

--- org.glite.wms.wmproxy-api-api-cpp/examples/glite-wmp-job-submit_JSDL.cpp
+++ org.glite.wms.wmproxy-api-api-cpp/examples/glite-wmp-job-submit_JSDL.cpp
@@ -1,12 +1,20 @@
 /*
- *       Copyright (c) Members of the EGEE Collaboration. 2004.
- *      See http://public.eu-egee.org/partners/ for details on the copyright holders.
- *       For license conditions see the license file or http://www.eu-egee.org/license.html
- *
- * 	Filename  : 	glite-wmp-job-submit_JSDL.cpp
- * 	Author    : 	Marco Sottilaro <marco.sottilaro@datamat.it>
- * 	Copyright : 	(c) 2004 by Datamat S.p.A.
- *
+*	Copyright (c) Members of the EGEE Collaboration. 2004.
+*	See http://www.eu-egee.org/partners/ for details on the
+*	copyright holders.
+*
+*	Licensed under the Apache License, Version 2.0 (the "License");
+*	you may not use this file except in compliance with the License.
+*	You may obtain a copy of the License at
+*	 
+*	     http://www.apache.org/licenses/LICENSE-2.0
+*	 
+*	Unless required by applicable law or agreed to in writing, software
+*	distributed under the License is distributed on an "AS IS" BASIS,
+*	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+*	either express or implied.
+*	See the License for the specific language governing permissions and
+*	limitations under the License.
 */
 
 #include "soapWMProxyProxy.h"
