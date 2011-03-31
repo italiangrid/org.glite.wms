cabcee14498b949a4b94e73f7a0dea814e7cab46
jwhite <jwhite>
2011-03-31 13:46:12 +0000

    Blanket changes for EMI  Modified Files:  	add-remove-localpap.sh en-disable-pap.sh pap-ping.sh  	refresh-cache.sh set-get-pap-orders.sh  	set-get-poll-interval.sh test-authz.sh update-pap.sh

--- org.glite.testsuites.ctb/Argus/tests/PAP-management/set-get-pap-orders.sh
+++ org.glite.testsuites.ctb/Argus/tests/PAP-management/set-get-pap-orders.sh
@@ -2,7 +2,35 @@
 
 failed="no"
 
-/etc/rc.d/init.d/pap-standalone status | grep -q 'PAP running'
+if [ -z $PAP_HOME ]
+then
+    if [ -d /usr/share/argus/pap ]
+    then
+        PAP_HOME=/usr/share/argus/pap
+    else
+        if [ -d /opt/argus/pap ]
+        then
+            PAP_HOME=/opt/argus/pap
+        else
+            echo "PAP_HOME not set, not found at standard locations. Exiting."
+            exit 2;
+        fi
+    fi
+fi
+PAP_CTRL=argus-pap
+if [ -f /etc/rc.d/init.d/pap-standalone ]
+then
+    PAP_CTRL=pap-standalone
+fi
+echo "PAP_CTRL set to: /etc/rc.d/init.d/$PAP_CTRL"
+/etc/rc.d/init.d/$PAP_CTRL status | grep -q 'PAP running'
+if [ $? -ne 0 ]; then
+  echo "PAP is not running"
+  /etc/rc.d/init.d/$PAP_CTRL start
+  sleep 10
+fi
+
+/etc/rc.d/init.d/$PAP_CTRL status | grep -q 'PAP running'
 if [ $? -ne 0 ]; then
   echo "PAP is not running"
   exit 1
--- org.glite.testsuites.ctb/Argus/tests/PAP-management/test-authz.sh
+++ org.glite.testsuites.ctb/Argus/tests/PAP-management/test-authz.sh
@@ -6,11 +6,40 @@
 echo `date`
 echo "---Test-argus---"
 
+if [ -z $PAP_HOME ]
+then
+    if [ -d /usr/share/argus/pap ]
+    then
+        PAP_HOME=/usr/share/argus/pap
+    else
+        if [ -d /opt/argus/pap ]
+        then
+            PAP_HOME=/opt/argus/pap
+        else
+            echo "PAP_HOME not set, not found at standard locations. Exiting."
+            exit 2;
+        fi
+    fi
+fi
+PAP_CTRL=argus-pap
+if [ -f /etc/rc.d/init.d/pap-standalone ]
+then
+    PAP_CTRL=pap-standalone
+fi
+echo "PAP_CTRL set to: /etc/rc.d/init.d/$PAP_CTRL"
+/etc/rc.d/init.d/$PAP_CTRL status | grep -q 'PAP running'
+if [ $? -ne 0 ]; then
+  echo "PAP is not running"
+  /etc/rc.d/init.d/$PAP_CTRL start
+  sleep 10
+fi
+
 argusconffile=$PAP_HOME/conf/pap_authorization.ini
 argusbkpfile=$PAP_HOME/conf/pap_authorization.bkp
 failed="no"
 
-/etc/rc.d/init.d/pap-standalone status | grep -q 'PAP running'
+
+/etc/rc.d/init.d/$PAP_CTRL status | grep -q 'PAP running'
 if [ $? -ne 0 ]; then
   echo "PAP is not running"
   exit 1
@@ -27,7 +56,7 @@ ANYONE : CONFIGURATION_READ
 [fqan]
 EOF
 
-/etc/rc.d/init.d/pap-standalone restart >>/dev/null
+/etc/rc.d/init.d/$PAP_CTRL restart >>/dev/null
 sleep 10
 $PAP_HOME/bin/pap-admin lp >>/dev/null
 if [ $? -eq 0 ]; then
@@ -40,11 +69,11 @@ fi
 #################################################################
 #start/restart the server
 mv -f $argusbkpfile $argusconffile
-/etc/rc.d/init.d/pap-standalone status | grep -q 'PAP not running'
+/etc/rc.d/init.d/$PAP_CTRL status | grep -q 'PAP not running'
 if [ $? -eq 0 ]; then
-  /etc/rc.d/init.d/pap-standalone start >>/dev/null
+  /etc/rc.d/init.d/$PAP_CTRL start >>/dev/null
 else
-  /etc/rc.d/init.d/pap-standalone restart >>/dev/null
+  /etc/rc.d/init.d/$PAP_CTRL restart >>/dev/null
 fi
 sleep 10
 
@@ -59,7 +88,7 @@ ANYONE : ALL
 [fqan]
 EOF
 
-/etc/rc.d/init.d/pap-standalone restart >>/dev/null
+/etc/rc.d/init.d/$PAP_CTRL restart >>/dev/null
 sleep 10
 $PAP_HOME/bin/pap-admin lp >>/dev/null
 if [ $? -ne 0 ]; then
@@ -72,11 +101,11 @@ fi
 #################################################################
 #start/restart the server
 mv -f $argusbkpfile $argusconffile
-/etc/rc.d/init.d/pap-standalone status | grep -q 'PAP not running'
+/etc/rc.d/init.d/$PAP_CTRL status | grep -q 'PAP not running'
 if [ $? -eq 0 ]; then
-  /etc/rc.d/init.d/pap-standalone start >>/dev/null
+  /etc/rc.d/init.d/$PAP_CTRL start >>/dev/null
 else
-  /etc/rc.d/init.d/pap-standalone restart >>/dev/null
+  /etc/rc.d/init.d/$PAP_CTRL restart >>/dev/null
 fi
 sleep 10
 
