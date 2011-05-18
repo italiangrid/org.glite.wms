de276c0329a4a62346f86f400ab7c1d94f83eb72
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2011-05-18 10:12:10 +0000

    Changes for gsoap 2.7.13

--- org.glite.wmproxy/src/server/wmproxy.cpp
+++ org.glite.wmproxy/src/server/wmproxy.cpp
@@ -93,7 +93,7 @@ using namespace std;
 const string opt_conf_file("glite_wms.conf");
 
 void
-sendFault(WMProxy &proxy, const string &method, const string &msg, int code)
+sendFault(WMProxyService &proxy, const string &method, const string &msg, int code)
 {
 	setSOAPFault(&proxy, code, method, time(NULL), code, msg);
 	soap_print_fault(&proxy, stderr);
@@ -189,27 +189,27 @@ main(int argc, char* argv[])
     } catch (configuration::CannotOpenFile &file) {
     	string msg = "Cannot open file: " + string(file.what());
 	    edglog(fatal)<<msg<<endl;
-	    WMProxy proxy;
+	    WMProxyService proxy;
 	   	sendFault(proxy, "main", msg, -5);
     } catch (configuration::CannotConfigure &error) {
     	string msg = "Cannot configure: " + string(error.what());
 	    edglog(fatal)<<msg<<endl;
-        WMProxy proxy;
+        WMProxyService proxy;
 	   	sendFault(proxy, "main", msg, -4);
 	} catch (wmsexception::Exception &exc) {
 		string msg = "Exception caught: " + string(exc.what());
 	    edglog(fatal)<<msg<<endl;
-		WMProxy proxy;
+		WMProxyService proxy;
 	   	sendFault(proxy, "main", msg, -3);
 	} catch (exception &ex) {
 		string msg = "Standard Exception caught: " + string(ex.what());
 	    edglog(fatal)<<msg<<endl;
-		WMProxy proxy;
+		WMProxyService proxy;
 	   	sendFault(proxy, "main", msg, -2);
  	} catch (...) {
     	string msg = "Uncaught exception";
 	    edglog(fatal)<<msg<<endl;
-		WMProxy proxy;
+		WMProxyService proxy;
 	   	sendFault(proxy, "main", msg, -1);
   	}
 	return 0;
--- org.glite.wmproxy/src/server/wmproxyserve.h
+++ org.glite.wmproxy/src/server/wmproxyserve.h
@@ -27,7 +27,7 @@ limitations under the License.
 #include "soapH.h"
 #include "soapWMProxyObject.h"
 
-class WMProxyServe:public WMProxy
+class WMProxyServe:public WMProxyService
 {
 	public:
 		
