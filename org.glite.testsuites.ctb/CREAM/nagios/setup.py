17faa420640fe17a7ad139040c26eede810e63e3
Paolo Andreetto <paolo.andreetto@pd.infn.it>
2012-01-05 10:59:36 +0000

    New definitions for setup.py

--- org.glite.testsuites.ctb/CREAM/nagios/setup.py
+++ org.glite.testsuites.ctb/CREAM/nagios/setup.py
@@ -2,5 +2,25 @@
 
 from distutils.core import setup
 
-setup(packages=['creammetrics'])
+comp_version = '1.0.0'
+
+probe_data = [
+              'src/CREAMCEDJS-probe',
+              'src/CREAMCE-probe',
+              'src/CREAMDJS-jdl.template',
+              'src/CREAM-jdl.template'
+             ]
+setup(
+      name='emi-cream-nagios',
+      version=comp_version,
+      description='Nagios probe for the EMI CREAM and WN services',
+      license='Apache Software License',
+      packages=['creammetrics'],
+      package_dir = {'': 'src'},
+      data_files=[
+                  ('etc/gridmon', ['etc/emi.nagios-jobmngt.conf']),
+                  ('usr/libexec/grid-monitoring/probes/emi.cream', probe_data),
+                  ('usr/share/doc/emi-cream-nagios-' + comp_version, ['CHANGES', 'README'])
+                 ]
+     )
 
