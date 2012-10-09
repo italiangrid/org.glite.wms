eac0b24c2761554d95f0b592238d484f78976e44
MarcoCecchi <marco.cecchi@cnaf.infn.it>
2012-10-09 18:54:18 +0200

    taken content from branch 3_3

--- org.glite.wms.nagios/setup.py
+++ org.glite.wms.nagios/setup.py
@@ -2,5 +2,17 @@
 
 from distutils.core import setup
 
-setup(packages=['wmsmetrics'])
+comp_version = '1.0.0'
 
+setup(
+      name="emi-wms-nagios",
+      version=comp_version,
+      description='Nagios probe for the EMI WMS service',
+      license='Apache Software License',
+      packages=['wmsmetrics'],
+      package_dir = {'': 'src'},
+      data_files=[
+                  ('libexec/grid-monitoring/probes/emi.wms', ['src/WMS-jdl.template', 'src/WMS-probe']),
+                  ('share/doc/emi-wms-nagios-' + comp_version, ['CHANGES', 'README'])
+                 ]
+     )
