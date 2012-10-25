f44cacb763d890f9232dc052162a180b8a6bb8a6
Alvise Dorigo <dorigoa@debian6-2.pd.infn.it>
2012-10-25 04:09:54 -0400

    temporary added config.h

---
+++ org.glite.wms.wmproxy-api-cpp/src/config.h
@@ -0,0 +1,92 @@
+/* src/autogen/config.h.  Generated from config.h.in by configure.  */
+/* src/autogen/config.h.in.  Generated from configure.ac by autoheader.  */
+/*
+ *  Define when to remove the stringstream use
+ *  from the boost library.
+ */
+/* #undef BOOST_NO_STRINGSTREAM */
+                                                                                
+/*
+ *  Define when we are sure to have the
+ *  right strstream header.
+ */
+/* #undef HAVE_STRINGSTREAM */
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the `memset' function. */
+#define HAVE_MEMSET 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Define to 1 if your C compiler doesn't accept -c and -o together. */
+/* #undef NO_MINUS_C_MINUS_O */
+
+/* Name of package */
+#define PACKAGE "glite-wms-wmproxy-api-cpp"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "GLite WMS wmproxy-api-cpp"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "GLite WMS wmproxy-api-cpp 3.3.3"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "glite-wms-wmproxy-api-cpp"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "3.3.3"
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
+
+/* Version number of package */
+#define VERSION "3.3.3"
+
+/* For Ipv6 support */
+#define WITH_IPV6 1
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
